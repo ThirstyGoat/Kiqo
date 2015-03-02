03072ec8093e4ce4cefaa7b111454d8450f2636d
Amy Martin <akm96@canterbury.ac.nz>
2015-03-02 14:40:02 +1300

    added project files

---
+++ src/main/java/seng302/group0/App.java
@@ -0,0 +1,13 @@
+package seng302.group0;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
