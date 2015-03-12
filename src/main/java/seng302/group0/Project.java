5540b8c8b5babb2d315ceb9cd65ffd71ea0f367f
Sam Schofield <sds53@uclive.ac.nz>
2015-03-12 16:47:08 +1300

    Added Project structure branch and implemented the Project class + tests

---
+++ src/main/java/seng302/group0/Project.java
@@ -0,0 +1,129 @@
+package seng302.group0;
+
+/**
+ * Created by samschofield on 12/03/15.
+ * Project class represents a software project
+ */
+public class Project {
+    private String shortName;
+    private String longName;
+    private String description;
+    private String saveLocation;
+
+    /**
+     * Create new Project
+     * @param shortName a unique short name for the project
+     * @param longName long name for project
+     * @param saveLocation save location for the project
+     */
+    public Project(String shortName, String longName, String saveLocation) {
+        this.shortName = shortName;
+        this.longName = longName;
+        this.saveLocation = saveLocation;
+    }
+
+    /**
+     * Create a new project
+     * @param shortName a unique short name for the project
+     * @param longName long name for project
+     * @param saveLocation save location for the project
+     * @param description description of the project
+     */
+    public Project(String shortName, String longName, String saveLocation, String description) {
+        this.shortName = shortName;
+        this.longName = longName;
+        this.description = description;
+        this.saveLocation = saveLocation;
+    }
+
+    /**
+     *
+     * @return the short name of project
+     */
+    public String getShortName() {
+        return shortName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Project project = (Project) o;
+
+        if (!shortName.equals(project.shortName)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return shortName.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "Project{" +
+                "shortName='" + shortName + '\'' +
+                ", longName='" + longName + '\'' +
+                ", description='" + description + '\'' +
+                ", saveLocation='" + saveLocation + '\'' +
+                '}';
+    }
+
+    /**
+     *
+     * @param shortName the short name of the project
+     */
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    /**
+     *
+     * @return longName the long name of the project
+     */
+    public String getLongName() {
+        return longName;
+    }
+
+    /**
+     *
+     * @param longName the long name for the project
+     */
+    public void setLongName(String longName) {
+        this.longName = longName;
+    }
+
+    /**
+     *
+     * @return the description of the project
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     *
+     * @param description the description of the project
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     *
+     * @return save location of project
+     */
+    public String getSaveLocation() {
+        return saveLocation;
+    }
+
+    /**
+     *
+     * @param saveLocation save location of project
+     */
+    public void setSaveLocation(String saveLocation) {
+        this.saveLocation = saveLocation;
+    }
+}
---
+++ src/test/java/seng302/group0/ProjectTest.java
@@ -0,0 +1,44 @@
+package seng302.group0;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProjectTest {
+
+    Project project1;
+
+    @Before
+    public void setUp() throws Exception {
+        project1 = new Project("p1", "project1", "/home/");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    /**
+     * Given that the two projects have the same short name they will be equal
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testEquals() throws Exception {
+        Project project2 = new Project("p1", "project1", "/home/");
+        assertTrue("Project should be equal", project2.equals(project1));
+
+        project2.setLongName("project2");
+        assertTrue("Project should still be equal", project2.equals(project1));
+
+        project2.setShortName("p2");
+        assertFalse("Project should not be equal", project2.equals(project1));
+    }
+
+    @Test
+    public void testHashCode() throws Exception {
+
+    }
+}
