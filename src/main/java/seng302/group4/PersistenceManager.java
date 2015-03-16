ca771794b81d492a35a08b7b59f5029c91ef2d8f
Sam Schofield <sds53@uclive.ac.nz>
2015-03-17 12:48:54 +1300

    Added Project save and load funcitonalilty + started tests

--- src/main/java/seng302/group4/PersistenceManager.java
+++ src/main/java/seng302/group4/PersistenceManager.java
@@ -1,7 +1,10 @@
 package seng302.group4;
 
 
-import java.io.File;
+import java.io.*;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 /**
  * Created by samschofield on 17/03/15.
@@ -13,24 +16,47 @@ public class PersistenceManager {
      * @param filePath - the path to the location where the project is to be saved
      * @param project - the project to be saved
      */
-    public static void saveProject(String filePath, Project project) {
+    public static void saveProject(String filePath, Project project) throws IOException{
         filePath += "/" + project.getShortName();
-        File f = new File(filePath);
+        File file = new File(filePath);
 
+        Gson gson = new GsonBuilder().create();
+        Writer writer = new FileWriter(filePath + "/" + project.getShortName() + ".json");
 
-        if(f.exists()) {
+        if(file.exists()) {
             System.out.println("Project already exists.. Saving chagnes");
+            gson.toJson(project, writer);
+
         } else {
             System.out.println("Creating new project‰");
-            f.mkdir();
+            file.mkdir();
+            gson.toJson(project, writer);
         }
+        writer.close();
+    }
+
+    /**
+     * Loads the project from the given json file
+     * @param filePath
+     * @return
+     * @throws IOException
+     */
+    public static Project loadProject(File filePath) throws IOException{
+        Gson gson = new GsonBuilder().create();
+        BufferedReader br = new BufferedReader(new FileReader(filePath));
+        return gson.fromJson(br, Project.class);
     }
 
 
     public static void main(String args[]) {
-        Project p = new Project("p1", "Project1", new File("/aaaa"));
-        PersistenceManager.saveProject("/Users/samschofield/Documents/Uni/2ndPro/seng302/SaveLocation", p);
+//        Project p = new Project("p1", "Project1", new File("/aaaa"));
+        try {
+//            PersistenceManager.saveProject("/Users/samschofield/Documents/Uni/2ndPro/seng302/SaveLocation", p);
+            Project p = PersistenceManager.loadProject(new File("/Users/samschofield/Documents/Uni/2ndPro/seng302/SaveLocation/p1/p1.json"));
+            System.out.println(p.getShortName());
+        } catch (IOException o) {
 
+        }
     }
 
 }
