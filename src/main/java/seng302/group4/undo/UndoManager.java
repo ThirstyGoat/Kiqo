297ffe3f180f7c37e40c37da5b97c2766dc3e782
carina <cdb71@uclive.ac.nz>
2015-03-17 15:35:28 +1300

    Redo functionality and tests added

--- src/main/java/seng302/group4/undo/UndoManager.java
+++ src/main/java/seng302/group4/undo/UndoManager.java
@@ -1,20 +1,33 @@
 package seng302.group4.undo;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class UndoManager {
-    private final Stack<Command> commandStack = new Stack<Command>();
+    private final Deque<Command> undoStack = new ArrayDeque<>(), redoStack = new ArrayDeque<>();
+
+
 
     public Object doCommand(final Command command) {
-        this.commandStack.push(command);
+        this.undoStack.push(command);
+        redoStack.clear();
         System.out.println("Doing " + command.toString());
         return command.execute();
     }
 
     public void undoCommand() {
-        final Command command = this.commandStack.pop();
+        final Command command = this.undoStack.pop();
         command.undo();
+        this.redoStack.push(command);
 
         System.out.println("Undoing " + command.toString());
+    }
+
+    public void redoCommand() {
+        final Command command = this.redoStack.pop();
+        command.redo();
+        this.undoStack.push(command);
+
+        System.out.println("Redoing " + command.toString());
     }
 }
--- src/test/java/seng302/group4/undo/UndoManagerTest.java
+++ src/test/java/seng302/group4/undo/UndoManagerTest.java
@@ -3,6 +3,8 @@ package seng302.group4.undo;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.NoSuchElementException;
+
 public class UndoManagerTest {
     class MockCommand extends Command<Void> {
         boolean done = false;
@@ -21,18 +23,48 @@ public class UndoManagerTest {
         public void undo() {
             this.done = false;
         }
+
+        @Override
+        public void redo() {
+            this.done = true;
+        }
     }
+
 
     /**
      * Tests that UndoManager executes and undoes commands
      */
     @Test
-    public void test() {
+    public void testUndo() {
         final UndoManager undoManager = new UndoManager();
         final MockCommand cmd = new MockCommand();
         undoManager.doCommand(cmd);
         Assert.assertTrue("Command should be executed", cmd.isDone());
         undoManager.undoCommand();
         Assert.assertFalse("Command should be undone", cmd.isDone());
+    }
+
+    @Test
+    public void testRedo() {
+        final UndoManager undoManager = new UndoManager();
+        final MockCommand cmd = new MockCommand();
+        undoManager.doCommand(cmd);
+        Assert.assertTrue("Command should be executed", cmd.isDone());
+        undoManager.undoCommand();
+        Assert.assertFalse("Command should be undone", cmd.isDone());
+        undoManager.redoCommand();
+        Assert.assertTrue("Command should be redone", cmd.isDone());
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testFailedRedo() {
+        final UndoManager undoManager = new UndoManager();
+        undoManager.redoCommand();
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void testFailedUndo() {
+        final UndoManager undoManager = new UndoManager();
+        undoManager.undoCommand();
     }
 }
