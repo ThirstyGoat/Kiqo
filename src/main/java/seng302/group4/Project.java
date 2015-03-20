3379fd5183454db5393bf48229a8de7e3d512c5d
Bradley Kirwan <bjk60@uclive.ac.nz>
2015-03-20 14:04:07 +1300

    Made the application only allow one project at a time. Removed undo functionality on creating project.

--- src/main/java/seng302/group4/Project.java
+++ src/main/java/seng302/group4/Project.java
@@ -2,7 +2,6 @@ package seng302.group4;
 
 import java.io.File;
 import java.io.Serializable;
-import java.util.ArrayList;
 
 /**
  * Created by samschofield on 12/03/15. Project class represents a software
@@ -15,7 +14,6 @@ public class Project implements Serializable {
     private String longName;
     private String description;
     private File saveLocation;
-    private ArrayList<Person> people = new ArrayList<Person>();
 
     /**
      * No-args constructor for JavaBeans(TM) compliance. Use at your own risk.
@@ -59,22 +57,6 @@ public class Project implements Serializable {
         this.saveLocation = saveLocation;
     }
 
-    /**
-     *
-     * @param person - person to add to people list in project
-     */
-    public void addPerson(Person person) {
-        people.add(person);
-    }
-
-    /**
-     *
-     * @return arraylist of people in project
-     */
-    public ArrayList<Person> getPeople() {
-        return people;
-    }
-
     @Override
     public boolean equals(final Object o) {
         if (this == o) {
@@ -103,19 +85,45 @@ public class Project implements Serializable {
 
     /**
      *
+     * @return longName the long name of the project
+     */
+    public String getLongName() {
+        return this.longName;
+    }
+
+    /**
+     *
+     * @return save location of project
+     */
+    public File getSaveLocation() {
+        return this.saveLocation;
+    }
+
+    /**
+     *
+     * @return the short name of project
+     */
+    public String getShortName() {
+        return this.shortName;
+    }
+
+    @Override
+    public int hashCode() {
+        return this.shortName.hashCode();
+    }
+
+    public void prepareForDestruction() {
+        // FIXME Auto-generated method stub
+        // eg. remove people
+    }
+
+    /**
+     *
      * @param description
      *            the description of the project
      */
     public void setDescription(final String description) {
         this.description = description;
-    }
-
-    /**
-     *
-     * @return longName the long name of the project
-     */
-    public String getLongName() {
-        return this.longName;
     }
 
     /**
@@ -129,27 +137,11 @@ public class Project implements Serializable {
 
     /**
      *
-     * @return save location of project
-     */
-    public File getSaveLocation() {
-        return this.saveLocation;
-    }
-
-    /**
-     *
      * @param saveLocation
      *            save location of project
      */
     public void setSaveLocation(final File saveLocation) {
         this.saveLocation = saveLocation;
-    }
-
-    /**
-     *
-     * @return the short name of project
-     */
-    public String getShortName() {
-        return this.shortName;
     }
 
     /**
@@ -162,24 +154,9 @@ public class Project implements Serializable {
     }
 
     @Override
-    public int hashCode() {
-        return this.shortName.hashCode();
-    }
-
-    public void prepareForDestruction() {
-        // FIXME Auto-generated method stub
-        // eg. remove people
-    }
-
-    @Override
     public String toString() {
-        return "Project{" + "shortName='" + this.shortName + '\'' + ", longName='" + this.longName + '\'' + ", description='"
-                + this.description + '\'' + ", saveLocation='" + this.saveLocation + '\'' + '}';
-//        return shortName;
+        return this.shortName;
+//        return "Project{" + "shortName='" + this.shortName + '\'' + ", longName='" + this.longName + '\'' + ", description='"
+//                + this.description + '\'' + ", saveLocation='" + this.saveLocation + '\'' + '}';
     }
-
-    public void removePerson(Person person) {
-        people.remove(person);
-    }
-
 }
