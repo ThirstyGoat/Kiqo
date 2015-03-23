f17860a6aa6bf1df1a4140361d3194353464a2c5
Amy Martin <akm96@uclive.ac.nz>
2015-03-23 15:54:02 +1300

    Improved JSON serialisation.
    Made Project's saveLocation transient so it's not saved to JSON. Made
    PersistenceManager Exceptions more specific, updated unit tests to
    match.
    story:2

--- src/test/java/seng302/group4/PersistenceManagerTest.java
+++ src/test/java/seng302/group4/PersistenceManagerTest.java
@@ -1,66 +1,76 @@
 package seng302.group4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
-import org.junit.Test;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runners.MethodSorters;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.function.Predicate;
-
-import static org.junit.Assert.*;
-
+import com.google.gson.JsonSyntaxException;
 
 // Methods are run in alphabetical order
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class PersistenceManagerTest {
 
+    Project project = null;
+
     @Rule
     public TemporaryFolder testFolder = new TemporaryFolder();
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     /**
-     * Tests that a project can be saved then loaded.
-     * All in one test so that it works with the temp folder
+     * Tests that a project can be saved then loaded. All in one test so that it
+     * works with the temp folder
+     *
      * @throws Exception
      */
     @Test
-    public void aBestCase() throws Exception {
-        Project project = new Project("p", "Project", testFolder.newFile("test.json"));
-        PersistenceManager.saveProject(project.getSaveLocation(), project);
-        assertTrue(new File(project.getSaveLocation() + "").exists());
+    public void testLoad_happyCase() throws Exception {
+        this.project = new Project("p", "Project", this.testFolder.newFile("test.json"));
+        PersistenceManager.saveProject(this.project.getSaveLocation(), this.project);
+        Assert.assertTrue(new File(this.project.getSaveLocation() + "").exists());
 
-        Project loadedProject = PersistenceManager.loadProject(new File(testFolder.getRoot() + "/test.json"));
-        assertTrue(loadedProject.equals(project));
+        final Project loadedProject = PersistenceManager.loadProject(new File(this.testFolder.getRoot() + "/test.json"));
+        Assert.assertTrue(loadedProject.equals(this.project));
     }
 
     /**
-     * Tests that an exception is thrown when a invalid file path is given to load a project from
+     * Tests that attempting to load a non-existent project file throws a
+     * {@link FileNotFoundException}.
+     *
+     * @throws Exception
      */
-    @Rule public ExpectedException thrown=ExpectedException.none();
     @Test
-    public void bFileNotFoundLoad() throws Exception {
-        thrown.expect( Exception.class );
+    public void testLoad_fileNotFound() throws Exception {
+        this.thrown.expect(FileNotFoundException.class);
 
-        Project p = PersistenceManager.loadProject(new File("a/non/existent/file/path"));
-
+        final Project p = PersistenceManager.loadProject(new File("a/non/existent/file/path"));
     }
 
     /**
-     * Tests that people are saved with the project and can be loaded correctly
+     * Tests that attempting to load a badly-formed project file throws a
+     * {@link JsonSyntaxException}.
+     *
+     * @throws Exception
      */
+
     @Test
-    public void testSavePerson() throws Exception {
-        Person person = new Person("a", "a", "a", "a", "a", "a" ,"a");
-        Project project = new Project("p", "Project", testFolder.newFile("test.json"));
+    public void testLoad_invalidProjectFormat() throws Exception {
+        this.thrown.expect(JsonSyntaxException.class);
+        final File f = this.testFolder.newFile("test.json");
 
-        project.addPerson(person);
+        final FileWriter fw = new FileWriter(f);
+        fw.write("{"); // lone opening brace == bad json
+        fw.close();
 
-        PersistenceManager.saveProject(project.getSaveLocation(), project);
-        Project loadedProject = PersistenceManager.loadProject(project.getSaveLocation());
-
-        assertTrue(loadedProject.getPeople().contains(person));
+        final Project p = PersistenceManager.loadProject(f);
+        System.out.println(p);
     }
 }
