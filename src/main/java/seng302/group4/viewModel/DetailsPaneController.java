87b25b4f05528117febcfee0de281da2310ff3ad
carina <cdb71@uclive.ac.nz>
2015-03-25 12:43:44 +1300

    DetailsPane for Project created.  story:40

---
+++ src/main/java/seng302/group4/viewModel/DetailsPaneController.java
@@ -0,0 +1,19 @@
+package seng302.group4.viewModel;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * Created by Carina on 25/03/2015.
+ */
+public class DetailsPaneController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+
+}
