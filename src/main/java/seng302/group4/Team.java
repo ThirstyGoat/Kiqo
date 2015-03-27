dcc3a0b1572ecbff36252f4111a5ee3e41bfbe7d
Bradley Kirwan <bjk60@uclive.ac.nz>
2015-03-27 17:35:52 +1300

    Team model introduced

---
+++ src/main/java/seng302/group4/Team.java
@@ -0,0 +1,13 @@
+package seng302.group4;
+
+import java.io.Serializable;
+
+/**
+ * Created by bradley on 27/03/15.
+ */
+public class Team implements Serializable {
+    private String shortName;
+    private String description;
+
+
+}
