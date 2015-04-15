03df1ecb6d72eb7ba6774cd395742d17ee90906e
Amy Martin <akm96@uclive.ac.nz>
2015-04-15 15:41:19 +1200

    Added comments to test suite explaining what each method tests.

--- src/test/java/seng302/group4/undo/DeletePersonCommandTest.java
+++ src/test/java/seng302/group4/undo/DeletePersonCommandTest.java
@@ -17,12 +17,18 @@ public class DeletePersonCommandTest {
     private Project project;
     private Team team;
 
+    /**
+     * Sets up the test environment.
+     */
     @Before
     public void setUp() {
         project = new Project();
         team = new Team("", "", new ArrayList<>());
     }
 
+    /**
+     * Tests that the person is deleted from the project
+     */
     @Test
     public void deletePerson_PersonRemovedFromProject() {
         // Create new person
@@ -36,6 +42,9 @@ public class DeletePersonCommandTest {
         assert !project.getPeople().contains(person);
     }
 
+    /**
+     * Tests that the person is removed from all of their teams before being deleted
+     */
     @Test
     public void deletePerson_PersonRemovedFromTeam() {
         // Create new person
@@ -53,6 +62,9 @@ public class DeletePersonCommandTest {
         assert !team.getTeamMembers().contains(person);
     }
 
+    /**
+     * Tests that the PO is removed from their role before being deleted
+     */
     @Test
     public void deletePersonWithPORole_PersonRemovedFromTeamAndPORole() {
         // Create new person
@@ -72,6 +84,9 @@ public class DeletePersonCommandTest {
         assert team.getProductOwner() != person;
     }
 
+    /**
+     * Tests that a dev team member is removed from their role before being deleted
+     */
     @Test
     public void deletePersonWithDevRole_PersonRemovedFromTeamAndDevRole() {
         // Create new person
@@ -91,6 +106,9 @@ public class DeletePersonCommandTest {
         assert !team.getDevTeam().contains(person);
     }
 
+    /**
+     * Tests that the person is added back to their original project on undo.
+     */
     @Test
     public void undoDeletePerson_PersonAddedBackToProject() {
         // Create new person
@@ -108,6 +126,9 @@ public class DeletePersonCommandTest {
         assert project.getPeople().contains(person);
     }
 
+    /**
+     * Tests that the person is added back to their original team on undo.
+     */
     @Test
     public void undoDeletePersonInTeam_PersonAddedBackToTeam() {
         // Create new person
@@ -130,6 +151,9 @@ public class DeletePersonCommandTest {
         assert team.getTeamMembers().contains(person);
     }
 
+    /**
+     * Tests that the person is added back with their original PO role, on undo.
+     */
     @Test
     public void undoDeletePersonInTeamWithPORole_PersonAddedBackToTeamWithRole() {
         // Create new person
@@ -155,6 +179,9 @@ public class DeletePersonCommandTest {
         assert team.getProductOwner() == person;
     }
 
+    /**
+     * Tests that the person is added back with their original dev team role, on undo.
+     */
     @Test
     public void undoDeletePersonInTeamWithDevRole_PersonAddedBackToTeamWithDevRole() {
         // Create new person
