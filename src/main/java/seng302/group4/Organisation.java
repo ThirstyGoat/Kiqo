0d18624ce062e7dbd77cc69c493bdad9880fc04c
Amy Martin <akm96@uclive.ac.nz>
2015-04-22 18:02:10 +1200

    Began to move Release into Project (but left observableList inside Organisation for the tab's listView contents) story: 135

--- src/main/java/seng302/group4/Organisation.java
+++ src/main/java/seng302/group4/Organisation.java
@@ -3,9 +3,9 @@ package seng302.group4;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 
 /**
@@ -18,12 +18,12 @@ public class Organisation {
     private final ArrayList<Skill> skills = new ArrayList<>();
     private final ArrayList<Team> teams = new ArrayList<>();
     private final ArrayList<Person> people = new ArrayList<>();
-    private final ArrayList<Release> releases = new ArrayList<>();
     private transient ObservableList<Project> projectsObservable = FXCollections.observableList(projects);
     private transient ObservableList<Person> peopleObservable = FXCollections.observableList(people);
     private transient ObservableList<Skill> skillsObservable = FXCollections.observableList(skills);
     private transient ObservableList<Team> teamsObservable = FXCollections.observableList(teams);
-    private transient ObservableList<Release> releaseObservable = FXCollections.observableList(releases);
+    // even though releases are stored under Project, organisation has to keep track of them
+    private transient ObservableList<Release> releaseObservable = FXCollections.observableArrayList();
 
     private transient File saveLocation;
 
@@ -33,6 +33,35 @@ public class Organisation {
     public Organisation() {
         skills.add(poSkill);
         skills.add(smSkill);
+
+        // register listener on projects list to listen for changes in releases
+        // derived from http://docs.oracle.com/javafx/2/api/javafx/collections/ListChangeListener.Change.html
+        projectsObservable.addListener(new ListChangeListener<Project>() {
+            @Override
+            public void onChanged(javafx.collections.ListChangeListener.Change<? extends Project> c) {
+                while (c.next()) {
+                    if (c.wasPermutated()) {
+                        // ignore permutate
+                    } else if (c.wasUpdated() || c.wasReplaced()) {
+                        replaceReleaseList(c.getList());
+                    } else {
+                        for (final Project remitem : c.getRemoved()) {
+                            releaseObservable.removeAll(remitem.getReleases());
+                        }
+                        for (final Project additem : c.getAddedSubList()) {
+                            releaseObservable.addAll(additem.getReleases());
+                        }
+                    }
+                }
+            }
+
+            private void replaceReleaseList(ObservableList<? extends Project> list) {
+                releaseObservable.clear();
+                for (final Project project : list) {
+                    releaseObservable.addAll(project.getReleases());
+                }
+            }
+        });
     }
 
     public Organisation(final File saveLocation) {
@@ -109,15 +138,15 @@ public class Organisation {
         peopleObservable = FXCollections.observableList(people);
         skillsObservable = FXCollections.observableList(skills);
         teamsObservable = FXCollections.observableList(teams);
-        releaseObservable = FXCollections.observableList(releases);
+        // releaseObservable = FXCollections.observableList(releases);
     }
 
     public static void main(String[] args) {
-        Organisation o = new Organisation(new File("/Users/samschofield/Desktop/org.json"));
+        final Organisation o = new Organisation(new File("/Users/samschofield/Desktop/org.json"));
         o.getPeople().add(new Person("sam", null, null, null, null, null, null, null));
         try {
             PersistenceManager.saveOrganisation(new File("/Users/samschofield/Desktop/org.json"), o);
-        } catch (IOException e) {
+        } catch (final IOException e) {
             e.printStackTrace();
         }
 
--- src/main/java/seng302/group4/Project.java
+++ src/main/java/seng302/group4/Project.java
@@ -1,6 +1,7 @@
 package seng302.group4;
 
-import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by samschofield on 22/04/15.
@@ -9,7 +10,7 @@ public class Project extends Item {
     private String shortName;
     private String longName;
     private String description;
-
+    private final ArrayList<Release> releases = new ArrayList<>();
 
     /**
      * Create new Project
@@ -80,12 +81,20 @@ public class Project extends Item {
         this.shortName = shortName;
     }
 
+    public List<Release> getReleases() {
+        return releases;
+    }
+
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
-        Project project = (Project) o;
+        final Project project = (Project) o;
 
         return shortName.equals(project.shortName);
 
