06400d8b52446cf1625e111d9d0f3e4c77ed9b33
amy <amy@amy-HP-ENVY-14-SPECTRE-Notebook-PC>
2015-05-15 14:21:31 +1200

    Added CreateStoryCommand and unit tests. Currently broken because project doesn't have a getStories() method yet.
    story:139

---
+++ src/main/java/com/thirstygoat/kiqo/command/CreateStoryCommand.java
@@ -0,0 +1,36 @@
+package com.thirstygoat.kiqo.command;
+
+import com.thirstygoat.kiqo.model.Story;
+
+/**
+ * Created by james on 11/04/15.
+ */
+public class CreateStoryCommand extends Command<Story> {
+    private final Story story;
+
+
+    public CreateStoryCommand(final Story story) {
+        this.story = story;
+    }
+
+    @Override
+    public Story execute() {
+        story.getProject().observableStories().add(story);
+        return story;
+    }
+
+    @Override
+    public void undo() {
+        story.getProject().observableStories().remove(story);
+    }
+
+    @Override
+    public String toString() {
+        return "<Create Story: \"" + story.getShortName() + "\">";
+    }
+
+    @Override
+    public String getType() {
+        return "Create Story";
+    }
+}
