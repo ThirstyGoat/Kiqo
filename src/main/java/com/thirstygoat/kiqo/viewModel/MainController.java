3a2c2e89de8b77048ddc44bca3259bcd92512254
carina <cdb71@uclive.ac.nz>
2015-05-15 15:41:01 +1200

    Edited the revert dialog message story: 138

--- src/main/java/com/thirstygoat/kiqo/viewModel/MainController.java
+++ src/main/java/com/thirstygoat/kiqo/viewModel/MainController.java
@@ -791,7 +791,7 @@ public class MainController implements Initializable {
         if (!changesSaved.get()) {
             final String[] options = {"Revert", "Cancel"};
             final String response = GoatDialog.createBasicButtonDialog(primaryStage, "Revert Project", "You have unsaved changes.",
-                    "Save As before reverting or you will lose these changes", options);
+                    "\"File > Save As\" before reverting or you will lose these changes.", options);
             if (response.equals("Revert")) {
                 revert();
             } else if (response.equals("Cancel")) {
