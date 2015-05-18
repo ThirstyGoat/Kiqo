400bb4ad710b454989f6d505fb1db8fac17f7d05
Amy Martin <akm96@uclive.ac.nz>
2015-05-18 12:34:24 +1200

    Fixed bash script and made it less brittle for the future (now uses maven filtering to construct the name of the jar).
    [task without a story]

--- src/main/scripts/Kiqo-1.0.sh
+++ src/main/scripts/Kiqo-1.0.sh
@@ -1,16 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
-JAR_NAME=Kiqo-1.0-shaded
+JAR_PATH=../${project.artifactId}-${project.version}.jar
 
 # the debian-specific command "update-alternatives" lists all installed java commands
 java8_path=$(update-alternatives --list java | grep java-8)
 
 if [ $java8_path ]
     then
-        $java8_path -jar $JAR_NAME.jar
+        $java8_path -jar $JAR_PATH
     else echo "Java 8 does not appear to be installed on this machine.
 Please install Java 8 from http://java.com/en/download/."
 fi
 
 return 0
-
