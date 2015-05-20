a3a4d4fb849fa3d1c13f2392096a080ffe240591
carina <cdb71@uclive.ac.nz>
2015-05-20 16:20:41 +1200

    Implemented the CreateBacklogCommand and wrote the CreateBacklogCommandTest to go with this. story: 171

---
+++ src/test/java/com/thirstygoat/kiqo/command/CreateBacklogCommandTest.java
@@ -0,0 +1,45 @@
+package com.thirstygoat.kiqo.command;
+
+import com.thirstygoat.kiqo.model.Backlog;
+import com.thirstygoat.kiqo.model.Person;
+import com.thirstygoat.kiqo.model.Project;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Carina on 20/05/2015.
+ */
+public class CreateBacklogCommandTest {
+
+    private Backlog backlog;
+    private Project project;
+    private CreateBacklogCommand command;
+
+    @Before
+    public void setup() {
+        project = new Project("proj", "Project");
+        backlog = new Backlog("", "", "", new Person() ,project, new ArrayList<>() );
+        command = new CreateBacklogCommand(backlog);
+    }
+
+    @Test
+    public void createBacklog_BacklogAddedToProject() {
+        Assert.assertFalse(project.getBacklogs().contains(backlog));
+
+        command.execute();
+
+        Assert.assertTrue(project.getBacklogs().contains(backlog));
+    }
+
+    @Test
+    public void undoCreateBacklog_BacklogRemovedFromProject() {
+        command.execute();
+        command.undo();
+
+        Assert.assertFalse(project.getBacklogs().contains(backlog));
+    }
+
+}
