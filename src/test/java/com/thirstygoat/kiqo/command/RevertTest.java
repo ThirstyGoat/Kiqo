8dd374252a0667a4b423a7d9c20b0911ef3283e9
Sam Schofield <sds53@uclive.ac.nz>
2015-06-26 15:09:08 +1200

    Changed unit tests to use Assert.assert...()
    Story:215 pair[sds53, jjh72]

--- src/test/java/com/thirstygoat/kiqo/command/RevertTest.java
+++ src/test/java/com/thirstygoat/kiqo/command/RevertTest.java
@@ -2,19 +2,20 @@ package com.thirstygoat.kiqo.command;
 
 import com.thirstygoat.kiqo.model.Organisation;
 import com.thirstygoat.kiqo.model.Skill;
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 
 /**
  * Created by james on 26/06/15.
  */
-public class RevertTest extends TestCase {
+public class RevertTest {
     private UndoManager undoManager;
     private Organisation organisation;
 
-
+    @Before
     public void setUp() throws Exception {
-        super.setUp();
         organisation = new Organisation();
         undoManager = new UndoManager();
         for (int i = 0; i < 10; i++) {
@@ -27,15 +28,17 @@ public class RevertTest extends TestCase {
     /**
      * Revert from the top of the stack, branch and save should be 0
      */
+    @Test
     public void testBasicRevertFlag() {
         undoManager.revert();
-        assertTrue(undoManager.savePosition == 0);
-        assertTrue(undoManager.branchPosition == 0);
+        Assert.assertTrue(undoManager.savePosition == 0);
+        Assert.assertTrue(undoManager.branchPosition == 0);
     }
 
     /**
      * Save as stack size of 10, undo 5 times, revert, savePosition should be 10, branch 5
      */
+    @Test
     public void testLessBasicRevertFlag() {
         undoManager.markSavePosition();
         undoManager.undoCommand();
@@ -44,14 +47,15 @@ public class RevertTest extends TestCase {
         undoManager.undoCommand();
         undoManager.undoCommand();
         undoManager.revert();
-        assertTrue(undoManager.savePosition == 10);
-        assertTrue(undoManager.branchPosition == 5);
+        Assert.assertTrue(undoManager.savePosition == 10);
+        Assert.assertTrue(undoManager.branchPosition == 5);
     }
 
 
     /**
      * Have full stack, undo, undo, undo, doCommand (so it has branched), doCommand and branchPosition should be 7
      */
+    @Test
     public void testBranchPositionAfterBranching() {
         undoManager.markSavePosition();
         undoManager.undoCommand();
@@ -63,13 +67,14 @@ public class RevertTest extends TestCase {
         Command<Skill> newSkillCommand2 = new CreateSkillCommand(newSkill2, organisation);
         undoManager.doCommand(newSkillCommand);
         undoManager.doCommand(newSkillCommand2);
-        assertTrue(undoManager.branchPosition == 7);
+        Assert.assertTrue(undoManager.branchPosition == 7);
     }
 
     /**
      * Have full stack, undo, undo, undo, doCommand (so it has branched), doCommand and branchPosition should be 7
      * BEST FUNCTION NAME EVER
      */
+    @Test
     public void testBranchPositionAfterBranchingAndUndoingPastThatPoint() {
         undoManager.markSavePosition();
         undoManager.undoCommand();
@@ -85,21 +90,23 @@ public class RevertTest extends TestCase {
         undoManager.undoCommand();
         undoManager.undoCommand();
         System.out.println(undoManager.branchPosition);
-        assertTrue(undoManager.branchPosition == 6);
+        Assert.assertTrue(undoManager.branchPosition == 6);
     }
 
     /**
      * Test revert when past the save position
      */
+    @Test
     public void testBasicRevert() {
         undoManager.revert();
-        assertTrue(undoManager.undoStack.size() == 0);
-        assertTrue(undoManager.redoStack.size() == 0);
+        Assert.assertTrue(undoManager.undoStack.size() == 0);
+        Assert.assertTrue(undoManager.redoStack.size() == 0);
     }
 
     /**
      * Test revert when saved, done commands then reverted
      */
+    @Test
     public void testBasicRevertPastSavePos() {
         undoManager.markSavePosition();
         Skill newSkill = new Skill("10", "");
@@ -110,30 +117,31 @@ public class RevertTest extends TestCase {
         undoManager.doCommand(newSkillCommand2);
         undoManager.revert();
 
-        assertTrue(undoManager.undoStack.size() == 10);
-        assertTrue(undoManager.redoStack.size() == 0);
-        assertTrue(undoManager.revertStack.size() == 0);
+        Assert.assertTrue(undoManager.undoStack.size() == 10);
+        Assert.assertTrue(undoManager.redoStack.size() == 0);
+        Assert.assertTrue(undoManager.revertStack.size() == 0);
 
         for (int i = 9; i > 0; i--) {
-            assertTrue(((CreateSkillCommand)undoManager.undoStack.pop()).skill.getShortName().equals(String.valueOf(i)));
+            Assert.assertTrue(((CreateSkillCommand) undoManager.undoStack.pop()).skill.getShortName().equals(String.valueOf(i)));
         }
     }
 
     /**
      * Test revert when saved, undone commands then reverted
      */
+    @Test
     public void testBasicRevertBeforeSavePos() {
         undoManager.markSavePosition();
         undoManager.undoCommand();
         undoManager.undoCommand();
         undoManager.revert();
 
-        assertTrue(undoManager.undoStack.size() == 10);
-        assertTrue(undoManager.redoStack.size() == 0);
-        assertTrue(undoManager.revertStack.size() == 0);
+        Assert.assertTrue(undoManager.undoStack.size() == 10);
+        Assert.assertTrue(undoManager.redoStack.size() == 0);
+        Assert.assertTrue(undoManager.revertStack.size() == 0);
 
         for (int i = 9; i > 0; i--) {
-            assertTrue(((CreateSkillCommand)undoManager.undoStack.pop()).skill.getShortName().equals(String.valueOf(i)));
+            Assert.assertTrue(((CreateSkillCommand) undoManager.undoStack.pop()).skill.getShortName().equals(String.valueOf(i)));
         }
     }
 
@@ -141,6 +149,7 @@ public class RevertTest extends TestCase {
      * Test revert when saved, undone commands then do commands then revert
      * do do do save undo undo do revert
      */
+    @Test
     public void testBranchRevert() {
         undoManager.markSavePosition();
         Skill newSkill = new Skill("10", "");
@@ -158,12 +167,12 @@ public class RevertTest extends TestCase {
 
         undoManager.revert();
 
-        assertTrue(undoManager.undoStack.size() == 10);
-        assertTrue(undoManager.redoStack.size() == 0);
-        assertTrue(undoManager.revertStack.size() == 0);
+        Assert.assertTrue(undoManager.undoStack.size() == 10);
+        Assert.assertTrue(undoManager.redoStack.size() == 0);
+        Assert.assertTrue(undoManager.revertStack.size() == 0);
 
         for (int i = 9; i > 0; i--) {
-            assertTrue(((CreateSkillCommand)undoManager.undoStack.pop()).skill.getShortName().equals(String.valueOf(i)));
+            Assert.assertTrue(((CreateSkillCommand) undoManager.undoStack.pop()).skill.getShortName().equals(String.valueOf(i)));
         }
     }
 
@@ -171,6 +180,7 @@ public class RevertTest extends TestCase {
      * Test revert when saved, undone commands then do commands then revert
      * do do do save undo undo do do undo do revert
      */
+    @Test
     public void testBranchBranchRevert() {
         undoManager.markSavePosition();
         Skill newSkill = new Skill("10", "");
@@ -195,12 +205,12 @@ public class RevertTest extends TestCase {
 
         undoManager.revert();
 
-        assertTrue(undoManager.undoStack.size() == 10);
-        assertTrue(undoManager.redoStack.size() == 0);
-        assertTrue(undoManager.revertStack.size() == 0);
+        Assert.assertTrue(undoManager.undoStack.size() == 10);
+        Assert.assertTrue(undoManager.redoStack.size() == 0);
+        Assert.assertTrue(undoManager.revertStack.size() == 0);
 
         for (int i = 9; i > 0; i--) {
-            assertTrue(((CreateSkillCommand)undoManager.undoStack.pop()).skill.getShortName().equals(String.valueOf(i)));
+            Assert.assertTrue(((CreateSkillCommand) undoManager.undoStack.pop()).skill.getShortName().equals(String.valueOf(i)));
         }
     }
 
@@ -208,6 +218,7 @@ public class RevertTest extends TestCase {
      *
      * do do do save undo undo save do do revert
      */
+    @Test
     public void testBranchRevertWithSaves() {
         undoManager.markSavePosition();
         Skill newSkill = new Skill("10", "");
@@ -225,12 +236,12 @@ public class RevertTest extends TestCase {
 
         undoManager.revert();
 
-        assertTrue(undoManager.undoStack.size() == 8);
-        assertTrue(undoManager.redoStack.size() == 0);
-        assertTrue(undoManager.revertStack.size() == 0);
+        Assert.assertTrue(undoManager.undoStack.size() == 8);
+        Assert.assertTrue(undoManager.redoStack.size() == 0);
+        Assert.assertTrue(undoManager.revertStack.size() == 0);
 
         for (int i = 7; i > 0; i--) {
-            assertTrue(((CreateSkillCommand)undoManager.undoStack.pop()).skill.getShortName().equals(String.valueOf(i)));
+            Assert.assertTrue(((CreateSkillCommand) undoManager.undoStack.pop()).skill.getShortName().equals(String.valueOf(i)));
         }
     }
 
@@ -238,6 +249,7 @@ public class RevertTest extends TestCase {
      *
      * do do do save undo undo revert do do revert
      */
+    @Test
     public void testBranchRevertAndGoPastSavePos() {
         undoManager.markSavePosition();
         Skill newSkill = new Skill("10", "");
@@ -255,15 +267,15 @@ public class RevertTest extends TestCase {
 
         undoManager.revert();
 
-        assertTrue(undoManager.undoStack.size() == 10);
-        assertTrue(undoManager.redoStack.size() == 0);
-        assertTrue(undoManager.revertStack.size() == 0);
+        Assert.assertTrue(undoManager.undoStack.size() == 10);
+        Assert.assertTrue(undoManager.redoStack.size() == 0);
+        Assert.assertTrue(undoManager.revertStack.size() == 0);
 
         System.out.println("undo: " + undoManager.undoStack.size());
         System.out.println("redo: " + undoManager.redoStack.size());
         System.out.println("revert: " + undoManager.revertStack.size());
         for (int i = 9; i > 0; i--) {
-            assertTrue(((CreateSkillCommand)undoManager.undoStack.pop()).skill.getShortName().equals(String.valueOf(i)));
+            Assert.assertTrue(((CreateSkillCommand) undoManager.undoStack.pop()).skill.getShortName().equals(String.valueOf(i)));
         }
     }
 
