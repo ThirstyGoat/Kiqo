a81a1f61bb2485795cad7223e8288c0ca7709a21
Amy Martin <akm96@uclive.ac.nz>
2015-07-17 13:22:48 +1200

    Switched order of parameters (we didn't spot it because it's not compiling yet anyway -- downsides of TDD).
    story:204
    pair[lih18]

--- src/test/java/com/thirstygoat/kiqo/viewModel/StoryDialogViewModelTest.java
+++ src/test/java/com/thirstygoat/kiqo/viewModel/StoryDialogViewModelTest.java
@@ -13,17 +13,17 @@ public class StoryDialogViewModelTest {
     public void testShortNameValidation() {
         StoryDialogViewModel storyDialogViewModel = new StoryDialogViewModel();
         
-        Assert.assertFalse(storyDialogViewModel.shortNameIsValid(), "Must not be valid initially.");
+        Assert.assertFalse("Must not be valid initially.", storyDialogViewModel.shortNameIsValid());
         
         // form
         storyDialogViewModel.shortNameProperty().set("Billy Goat");
-        Assert.assertTrue(storyDialogViewModel.shortNameIsValid(), "Valid input not recognised as valid.");
+        Assert.assertTrue("Valid input not recognised as valid.",storyDialogViewModel.shortNameIsValid());
 
         storyDialogViewModel.shortNameProperty().set("");
-        Assert.assertFalse(storyDialogViewModel.shortNameIsValid(), "Must not be an empty string.");
+        Assert.assertFalse("Must not be an empty string.", storyDialogViewModel.shortNameIsValid());
 
         storyDialogViewModel.shortNameProperty().set("This name is longer than 20 characters.");
-        Assert.assertFalse(storyDialogViewModel.shortNameIsValid(), "Must not be longer than 20 characters.");
+        Assert.assertFalse("Must not be longer than 20 characters.", storyDialogViewModel.shortNameIsValid());
         
         // uniqueness within project
         Person creator = new Person("person shortName", "longName", "description", "userId", "email", "phone", "dept", new ArrayList<Skill>());
@@ -31,46 +31,46 @@ public class StoryDialogViewModelTest {
         Story story = new Story("story shortName", "longName", "description", creator, project, null, 0, 0, Scale.FIBONACCI);
 
         storyDialogViewModel.shortNameProperty().set("story shortName unique");
-        Assert.assertTrue(storyDialogViewModel.shortNameIsValid(), "Unique short name not recognised as valid.");
+        Assert.assertTrue("Unique short name not recognised as valid.", storyDialogViewModel.shortNameIsValid());
         storyDialogViewModel.shortNameProperty().set("story shortName");
-        Assert.assertFalse(storyDialogViewModel.shortNameIsValid(), "Must be unique within project.");
+        Assert.assertFalse("Must be unique within project.", storyDialogViewModel.shortNameIsValid());
     }
     
     @Test
     public void testLongNameValidation() {
         StoryDialogViewModel storyDialogViewModel = new StoryDialogViewModel();
         
-        Assert.assertFalse(storyDialogViewModel.longNameIsValid(), "Must not be valid initially.");
+        Assert.assertFalse("Must not be valid initially.", storyDialogViewModel.longNameIsValid());
         
         storyDialogViewModel.longNameProperty().set("Billy Goat");
-        Assert.assertTrue(storyDialogViewModel.longNameIsValid(), "Valid input not recognised as valid.");
+        Assert.assertTrue("Valid input not recognised as valid.", storyDialogViewModel.longNameIsValid());
 
         storyDialogViewModel.longNameProperty().set("");
-        Assert.assertFalse(storyDialogViewModel.longNameIsValid(), "Must not be an empty string.");
+        Assert.assertFalse("Must not be an empty string.", storyDialogViewModel.longNameIsValid());
     }
 
     @Test
     public void testCreatorValidation() {
         StoryDialogViewModel storyDialogViewModel = new StoryDialogViewModel();
 
-        Assert.assertFalse(storyDialogViewModel.creatorIsValid(), "Must not be valid initially.");
+        Assert.assertFalse("Must not be valid initially.", storyDialogViewModel.creatorIsValid());
 
         storyDialogViewModel.creatorProperty().set(null);
-        Assert.assertFalse(storyDialogViewModel.creatorIsValid(), "Must not be null.");
+        Assert.assertFalse("Must not be null.", storyDialogViewModel.creatorIsValid());
 
         Person creator = new Person("person shortName", "longName", "description", "userId", "email", "phone", "dept", new ArrayList<Skill>());
         storyDialogViewModel.creatorProperty().set(creator);
-        Assert.assertTrue(storyDialogViewModel.creatorIsValid(), "Valid creator not recognised as valid.");
+        Assert.assertTrue("Valid creator not recognised as valid.", storyDialogViewModel.creatorIsValid());
     }
 
     @Test
     public void testBacklogValidation() {
         StoryDialogViewModel storyDialogViewModel = new StoryDialogViewModel();
 
-        Assert.assertFalse(storyDialogViewModel.backlogIsValid(), "Must not be valid initially.");
+        Assert.assertFalse("Must not be valid initially.", storyDialogViewModel.backlogIsValid());
 
         storyDialogViewModel.backlogProperty().set(null);
-        Assert.assertFalse(storyDialogViewModel.backlogIsValid(), "Must not be null.");
+        Assert.assertFalse("Must not be null.", storyDialogViewModel.backlogIsValid());
     }
 
 }
