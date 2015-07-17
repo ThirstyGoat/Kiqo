8943cec49245e15160a06acca651c3f3da74cbc2
Amy Martin <akm96@uclive.ac.nz>
2015-07-18 11:34:50 +1200

    Replaced reflection with a non-related validation step, just to set a private field. Not sure which is worse.
    story:204

--- src/test/java/com/thirstygoat/kiqo/viewModel/StoryFormViewModelTest.java
+++ src/test/java/com/thirstygoat/kiqo/viewModel/StoryFormViewModelTest.java
@@ -14,57 +14,68 @@ public class StoryFormViewModelTest {
 
     @Test
     public void testShortNameValidation() throws NoSuchFieldException, IllegalAccessException {
-        StoryFormViewModel storyDialogViewModel = new StoryFormViewModel();
-        Predicate<String> predicate = storyDialogViewModel.getShortNameValidation();
+        StoryFormViewModel storyFormViewModel = new StoryFormViewModel();
+        Predicate<String> predicate = storyFormViewModel.getShortNameValidation();
 
-        Assert.assertFalse("Must not be valid initially.", predicate.test(storyDialogViewModel.shortNameProperty().get()));
+        Assert.assertFalse("Must not be valid initially.", predicate.test(storyFormViewModel.shortNameProperty().get()));
         Assert.assertTrue("Valid input not recognised as valid.", predicate.test("Billy Goat"));
         Assert.assertFalse("Must not be an empty string.", predicate.test(""));
         Assert.assertFalse("Must not be longer than 20 characters.", predicate.test("This name is longer than 20 characters."));
 
-        // uniqueness within project
+        // validating uniqueness within project requires model data
+        final String projectName = "project shortName";
+        final String storyName = "story shortName";
+        
         Person creator = new Person("person shortName", "longName", "description", "userId", "email", "phone", "dept", new ArrayList<Skill>());
-        Project project = new Project("project shortName", "longName");
-        final String sharedShortName = "story shortName";
-        Story story = new Story(sharedShortName, "longName", "description", creator, project, null, 0, Scale.FIBONACCI, 0);
+        Project project = new Project(projectName, "longName");
+        Story story = new Story(storyName, "longName", "description", creator, project, null, 0, Scale.FIBONACCI, 0);
         project.setUnallocatedStories(new ArrayList<Story>(Arrays.asList(story)));
-
+        
+        Organisation organisation = new Organisation();
+        organisation.getPeople().add(creator);
+        organisation.getProjects().add(project);
+        storyFormViewModel.setOrganisation(organisation);
+        
+        // workaround so that project field gets set
+        storyFormViewModel.projectNameProperty().set(projectName);
+        storyFormViewModel.getProjectValidation().test(storyFormViewModel.projectNameProperty().get());
+        
         // Because otherwise the project is null so will always return false
-        Field privateProject = storyDialogViewModel.getClass().getDeclaredField("project");
-        privateProject.setAccessible(true);
-        privateProject.set(storyDialogViewModel, project);
+//        Field privateProject = storyFormViewModel.getClass().getDeclaredField("project");
+//        privateProject.setAccessible(true);
+//        privateProject.set(storyFormViewModel, project);
 
         Assert.assertTrue("Unique short name not recognised as valid.", predicate.test("unique"));
-        Assert.assertFalse("Must be unique within project.", predicate.test(sharedShortName));
+        Assert.assertFalse("Must be unique within project.", predicate.test(storyName));
     }
 
-    //Todo do we need validation on long name and description?
+    // TODO do we need validation on long name and description?
 //    @Test
 //    public void testLongNameValidation() {
-//        StoryFormViewModel storyDialogViewModel = new StoryFormViewModel();
-//        Predicate<String> predicate = storyDialogViewModel.getLongNameValidation();
+//        StoryFormViewModel storyFormViewModel = new StoryFormViewModel();
+//        Predicate<String> predicate = storyFormViewModel.getLongNameValidation();
 //
-//        Assert.assertFalse("Must not be valid initially.", predicate.test(storyDialogViewModel.longNameProperty().get()));
+//        Assert.assertFalse("Must not be valid initially.", predicate.test(storyFormViewModel.longNameProperty().get()));
 //        Assert.assertTrue("Valid input not recognised as valid.", predicate.test("Billy Goat"));
 //        Assert.assertFalse("Must not be an empty string.", predicate.test(""));
 //    }
 
 //    @Test
 //    public void testDescriptionValidation() {
-//        StoryFormViewModel storyDialogViewModel = new StoryFormViewModel();
-//        Predicate<String> predicate = storyDialogViewModel.getDescriptionValidation();
+//        StoryFormViewModel storyFormViewModel = new StoryFormViewModel();
+//        Predicate<String> predicate = storyFormViewModel.getDescriptionValidation();
 //
-//        Assert.assertTrue("Description should be valid by default.", predicate.test(storyDialogViewModel.descriptionProperty().get()));
+//        Assert.assertTrue("Description should be valid by default.", predicate.test(storyFormViewModel.descriptionProperty().get()));
 //        Assert.assertTrue("Valid input not recognised as valid.", predicate.test("Billy Goat"));
 //        Assert.assertTrue("Empty string not recognised as valid.", predicate.test(""));
 //    }
 //
 //    @Test
 //    public void testCreatorValidation() {
-//        StoryFormViewModel storyDialogViewModel = new StoryFormViewModel();
-//        Predicate<String> predicate = storyDialogViewModel.getCreatorValidation();
+//        StoryFormViewModel storyFormViewModel = new StoryFormViewModel();
+//        Predicate<String> predicate = storyFormViewModel.getCreatorValidation();
 //
-//        Assert.assertFalse("Must not be valid initially.", predicate.test(storyDialogViewModel.creatorProperty().get()));
+//        Assert.assertFalse("Must not be valid initially.", predicate.test(storyFormViewModel.creatorProperty().get()));
 //        Assert.assertFalse("Must not be null.", predicate.test(null));
 //
 //        Person creator = new Person("person shortName", "longName", "description", "userId", "email", "phone", "dept", new ArrayList<Skill>());
@@ -73,18 +84,18 @@ public class StoryFormViewModelTest {
 //
 //    @Test
 //    public void testProjectValidation() {
-//        StoryFormViewModel storyDialogViewModel = new StoryFormViewModel();
-//        Predicate<String> predicate = storyDialogViewModel.getProjectValidation();
+//        StoryFormViewModel storyFormViewModel = new StoryFormViewModel();
+//        Predicate<String> predicate = storyFormViewModel.getProjectValidation();
 //
 //        Assert.fail("Not yet implemented");
 //    }
 //
 //    @Test
 //    public void testBacklogValidation() {
-//        StoryFormViewModel storyDialogViewModel = new StoryFormViewModel();
-//        Predicate<String> predicate = storyDialogViewModel.getBacklogValidation();
+//        StoryFormViewModel storyFormViewModel = new StoryFormViewModel();
+//        Predicate<String> predicate = storyFormViewModel.getBacklogValidation();
 //
-//        Assert.assertFalse("Must not be valid initially.", predicate.test(storyDialogViewModel.backlogProperty().get()));
+//        Assert.assertFalse("Must not be valid initially.", predicate.test(storyFormViewModel.backlogProperty().get()));
 //        Assert.assertFalse("Must not be null.", predicate.test(null));
 //
 //        // Setup objects for testing cases in which backlog belongs to project and does not belong to a project.
@@ -94,7 +105,7 @@ public class StoryFormViewModelTest {
 //        Backlog backlog1 = new Backlog("backlog in the same project", "longName", "description", productOwner, project, new ArrayList<>(), Scale.FIBONACCI);
 //        Backlog backlog2 = new Backlog("backlog not in project", "longName", "description", productOwner, project, new ArrayList<>(), Scale.FIBONACCI);
 //        project.setBacklogs(Arrays.asList(backlog1));
-//        storyDialogViewModel.projectProperty().set(project); // TODO requires "protected ObjectProperty<Project> projectProperty"
+//        storyFormViewModel.projectProperty().set(project); // TODO requires "protected ObjectProperty<Project> projectProperty"
 //
 //        // Backlog belongs to selected project
 //        Assert.assertTrue("Valid backlog should be recognised as valid.", predicate.test(backlog1.getShortName()));
@@ -105,32 +116,32 @@ public class StoryFormViewModelTest {
 //
 //    @Test
 //    public void testPriorityValidation() {
-//        StoryFormViewModel storyDialogViewModel = new StoryFormViewModel();
-//        Predicate<String> predicate = storyDialogViewModel.getPriorityValidation();
+//        StoryFormViewModel storyFormViewModel = new StoryFormViewModel();
+//        Predicate<String> predicate = storyFormViewModel.getPriorityValidation();
 //
 //        Assert.fail("Not yet implemented");
 //    }
 //
 //    @Test
 //    public void testAcceptanceCriteriaValidation() {
-//        StoryFormViewModel storyDialogViewModel = new StoryFormViewModel();
-//        Predicate<String> predicate = storyDialogViewModel.getAcceptanceCriteriaValidation();
+//        StoryFormViewModel storyFormViewModel = new StoryFormViewModel();
+//        Predicate<String> predicate = storyFormViewModel.getAcceptanceCriteriaValidation();
 //
 //        Assert.fail("Not yet implemented");
 //    }
 //
 //    @Test
 //    public void testScaleValidation() {
-//        StoryFormViewModel storyDialogViewModel = new StoryFormViewModel();
-//        Predicate<String> predicate = storyDialogViewModel.getScaleValidation();
+//        StoryFormViewModel storyFormViewModel = new StoryFormViewModel();
+//        Predicate<String> predicate = storyFormViewModel.getScaleValidation();
 //
 //        Assert.fail("Not yet implemented");
 //    }
 //
 //    @Test
 //    public void testEstimateValidation() {
-//        StoryFormViewModel storyDialogViewModel = new StoryFormViewModel();
-//        Predicate<String> predicate = storyDialogViewModel.getEstimateValidation();
+//        StoryFormViewModel storyFormViewModel = new StoryFormViewModel();
+//        Predicate<String> predicate = storyFormViewModel.getEstimateValidation();
 //
 //        Assert.fail("Not yet implemented");
 //    }
