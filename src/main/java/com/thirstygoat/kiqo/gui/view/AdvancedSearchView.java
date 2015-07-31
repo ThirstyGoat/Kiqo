95766edeb8d98f8f2d64d7a222f35d6a61d6b04e
Bradley Kirwan <bjk60@uclive.ac.nz>
2015-07-31 21:40:31 +1200

    Fixed issue involving size of Advanced Search details pane. Implemented previous functioning search functionality into Advanced Search. story:281

--- src/main/java/com/thirstygoat/kiqo/gui/view/AdvancedSearchView.java
+++ src/main/java/com/thirstygoat/kiqo/gui/view/AdvancedSearchView.java
@@ -1,8 +1,20 @@
 package com.thirstygoat.kiqo.gui.view;
 
 import com.thirstygoat.kiqo.gui.model.AdvancedSearchViewModel;
+import com.thirstygoat.kiqo.search.SearchResult;
 import de.saxsys.mvvmfx.FxmlView;
+import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,8 +23,33 @@ import java.util.ResourceBundle;
  * Created by bradley on 31/07/15.
  */
 public class AdvancedSearchView implements FxmlView<AdvancedSearchViewModel>, Initializable {
+
+    @FXML
+    private TextField searchTextField;
+    @FXML
+    private Button searchButton;
+    @FXML
+    private ListView<SearchResult> resultsListView;
+    @InjectViewModel
+    private AdvancedSearchViewModel viewModel;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        resultsListView.setItems(viewModel.getSearchResults());
 
+        resultsListView.setCellFactory(param -> new ListCell<SearchResult>() {
+            @Override
+            protected void updateItem(SearchResult item, boolean empty) {
+                super.updateItem(item, empty);
+                if (!empty && item != null) {
+                    setText(item.getResultText());
+                } else {
+                    setText("");
+                }
+            }
+        });
+
+        // Set button action
+        searchButton.setOnAction(event -> viewModel.search(searchTextField.getText()));
     }
 }
