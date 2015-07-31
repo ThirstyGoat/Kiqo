9509704ba1c429d819d5f5365c536de885008cd5
Bradley Kirwan <bjk60@uclive.ac.nz>
2015-07-31 14:39:49 +1200

    Added "basic" advanced search view implementation.
    Created empty AdvancedSearchViewModel.
    Added Advanced Search Menu Item to Menubar.
    story:281

---
+++ src/main/java/com/thirstygoat/kiqo/gui/model/AdvancedSearchViewModel.java
@@ -0,0 +1,9 @@
+package com.thirstygoat.kiqo.gui.model;
+
+import de.saxsys.mvvmfx.ViewModel;
+
+/**
+ * Created by bradley on 31/07/15.
+ */
+public class AdvancedSearchViewModel implements ViewModel {
+}
