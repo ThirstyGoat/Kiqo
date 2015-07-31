d0f85b2be26e435136478a1dcfe72d5691b64f0d
Bradley Kirwan <bjk60@uclive.ac.nz>
2015-07-31 21:40:31 +1200

    Fixed issue involving size of Advanced Search details pane. Implemented previous functioning search functionality into Advanced Search. story:281

--- src/main/java/com/thirstygoat/kiqo/gui/model/AdvancedSearchViewModel.java
+++ src/main/java/com/thirstygoat/kiqo/gui/model/AdvancedSearchViewModel.java
@@ -1,9 +1,24 @@
 package com.thirstygoat.kiqo.gui.model;
 
+import com.thirstygoat.kiqo.search.Search;
+import com.thirstygoat.kiqo.search.SearchResult;
 import de.saxsys.mvvmfx.ViewModel;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 /**
  * Created by bradley on 31/07/15.
  */
 public class AdvancedSearchViewModel implements ViewModel {
+    private ObservableList<SearchResult> searchResults = FXCollections.observableArrayList();
+
+    public ObservableList<SearchResult> getSearchResults() {
+        return searchResults;
+    }
+
+    public void search(String query) {
+        final Search search = new Search(query);
+        searchResults.clear();
+        searchResults.addAll(search.execute());
+    }
 }
