68d6269f7e8fc2888e7d7a9759595dadeea01117
carina <cdb71@uclive.ac.nz>
2015-08-05 10:44:18 +1200

    Wrote the load function for SprintDetailsPaneViewModel story:280

---
+++ src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneViewModel.java
@@ -0,0 +1,47 @@
+package com.thirstygoat.kiqo.gui.sprint;
+
+import com.thirstygoat.kiqo.model.Sprint;
+
+/**
+ * Created by Carina Blair on 5/08/2015.
+ */
+public class SprintDetailsPaneViewModel extends SprintViewModel{
+    public final String PLACEHOLDER = "No stories in sprint";
+
+    public SprintDetailsPaneViewModel() {
+
+    }
+
+    /**
+     * Bind to model so there is not copying data in
+     */
+    @Override
+    public void load(Sprint sprint) {
+        if (sprint != null) {
+            super.goalProperty().bind(sprint.shortNameProperty());
+            super.longNameProperty().bind(sprint.longNameProperty());
+            super.descriptionProperty().bind(sprint.descriptionProperty());
+            super.backlogProperty().bind(sprint.backlogProperty());
+            super.startDateProperty().bind(sprint.startDateProperty());
+            super.endDateProperty().bind(sprint.endDateProperty());
+            super.teamProperty().bind(sprint.teamProperty());
+            super.releaseProperty().bind(sprint.releaseProperty());
+            super.getStories().clear();
+            super.getStories().addAll(sprint.getStories());
+        } else {
+            super.goalProperty().unbind();
+            super.longNameProperty().unbind();
+            super.descriptionProperty().unbind();
+            super.backlogProperty().unbind();
+            super.startDateProperty().unbind();
+            super.endDateProperty().unbind();
+            super.teamProperty().unbind();
+            super.releaseProperty().unbind();
+            super.getStories().clear();
+        }
+
+    }
+
+}
+
+
