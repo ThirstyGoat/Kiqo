f9f62c93272474a114219a27a736922530663222
Amy Martin <akm96@uclive.ac.nz>
2015-08-06 10:19:14 +1200

    Updated SearchTest to use CreateCommands instead of Item constructor. Converted SearchableItems.clear() into a static method (following singleton pattern).
    story:281

--- src/main/java/com/thirstygoat/kiqo/command/CreatePersonCommand.java
+++ src/main/java/com/thirstygoat/kiqo/command/CreatePersonCommand.java
@@ -9,7 +9,7 @@ import com.thirstygoat.kiqo.model.Person;
  * @author amy, Edited by Bradley 10/4/15
  *
  */
-public class CreatePersonCommand extends Command {
+public class CreatePersonCommand extends CreateCommand {
     private final Person person;
     private final Organisation organisation;
 
@@ -21,17 +21,18 @@ public class CreatePersonCommand extends Command {
      *            Project that person is to be added to
      */
     public CreatePersonCommand(final Person person, final Organisation organisation) {
+        super(person);
         this.person = person;
         this.organisation = organisation;
     }
 
     @Override
-    public void execute() {
+    public void addToModel() {
         organisation.getPeople().add(person);
     }
 
     @Override
-    public void undo() {
+    public void removeFromModel() {
         // Goodbye person
         // But we'll keep a reference to you just in case we have to redo later
         // on...
