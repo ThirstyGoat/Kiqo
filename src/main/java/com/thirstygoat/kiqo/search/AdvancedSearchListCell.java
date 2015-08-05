a057ad7998ccc197e885d832fd1da1a4cb44426c
Bradley Kirwan <bjk60@uclive.ac.nz>
2015-08-05 16:22:13 +1200

    Improved highlighting in advanced search. story:281

--- src/main/java/com/thirstygoat/kiqo/search/AdvancedSearchListCell.java
+++ src/main/java/com/thirstygoat/kiqo/search/AdvancedSearchListCell.java
@@ -3,6 +3,7 @@ package com.thirstygoat.kiqo.search;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.*;
 import javafx.scene.text.Font;
@@ -14,6 +15,9 @@ import java.util.Comparator;
  * Created by bradley on 1/08/15.
  */
 public class AdvancedSearchListCell extends ListCell<SearchResult> {
+
+    public static final int MAX_MATCH_DISPLAY_LENGTH = 100;
+
     @Override
     protected void updateItem(SearchResult item, boolean empty) {
         super.updateItem(item, empty);
@@ -35,8 +39,27 @@ public class AdvancedSearchListCell extends ListCell<SearchResult> {
 
         // For each string match, show it in the search results
         for (Match match : getItem().getMatches()) {
-            vBox.getChildren().add(new Label(match.getMatchedString() + " (" + match.getSimilarity() + " similarity)"));
+            vBox.getChildren().add(getHighlightedMatchNode(match));
         }
         return vBox;
+    }
+
+    private Node getHighlightedMatchNode(Match match) {
+        FlowPane flowPane = new FlowPane();
+        int lastIndex = 0;
+        for (int[] matchPos : match.getMatchPositions()) {
+            System.out.println(matchPos[1]);
+            if (matchPos[1] > 100)
+                break;
+
+            flowPane.getChildren().add(new Label(match.getMatchedString().substring(lastIndex, matchPos[0])));
+            Label highlightedLabel = new Label(match.getMatchedString().substring(matchPos[0], matchPos[1]));
+            highlightedLabel.setStyle("-fx-background-color: yellow");
+            flowPane.getChildren().add(highlightedLabel);
+            lastIndex = matchPos[1];
+        }
+        flowPane.getChildren().add(new Label(match.getMatchedString().substring(lastIndex, Math.min(MAX_MATCH_DISPLAY_LENGTH, match.getMatchedString().length()))));
+
+        return flowPane;
     }
 }
