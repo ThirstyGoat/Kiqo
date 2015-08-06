1fd0d1056730209fb088218e3c32ff4851f713a1
Sam Schofield <sds53@uclive.ac.nz>
2015-08-06 18:18:25 +1200

    Basic implementation of GoatLabel. Added it into skills details pane purely for testing purposes
    story:288

---
+++ src/main/java/com/thirstygoat/kiqo/gui/nodes/GoatLabelViewModel.java
@@ -0,0 +1,10 @@
+package com.thirstygoat.kiqo.gui.nodes;
+
+import de.saxsys.mvvmfx.ViewModel;
+
+/**
+ * Created by samschofield on 6/08/15.
+ */
+public class GoatLabelViewModel implements ViewModel {
+
+}
