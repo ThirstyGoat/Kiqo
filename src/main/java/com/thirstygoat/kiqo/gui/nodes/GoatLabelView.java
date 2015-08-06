2d4564d0ef9f88b11db64bcbef50c1dc4839ed41
Sam Schofield <sds53@uclive.ac.nz>
2015-08-06 21:46:43 +1200

    Improved the look of the Editable Label and added transitions
    story:280

--- src/main/java/com/thirstygoat/kiqo/gui/nodes/GoatLabelView.java
+++ src/main/java/com/thirstygoat/kiqo/gui/nodes/GoatLabelView.java
@@ -2,6 +2,9 @@ package com.thirstygoat.kiqo.gui.nodes;
 
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.animation.FadeTransition;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -9,6 +12,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.util.Duration;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -18,6 +22,17 @@ import java.util.ResourceBundle;
  * Created by samschofield on 6/08/15.
  */
 public class GoatLabelView implements FxmlView<GoatLabelViewModel>, Initializable {
+
+
+    @FXML
+    private HBox displayView;
+    @FXML
+    private HBox editView;
+    @FXML
+    private Button doneButton;
+
+
+
 
     @FXML
     private Label textLabel;
@@ -33,17 +48,50 @@ public class GoatLabelView implements FxmlView<GoatLabelViewModel>, Initializabl
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        textLabel.setText("some text");
+
+        displayView.setVisible(true);
+        editView.setVisible(false);
         textLabel.setVisible(true);
-        textInput.setVisible(false);
-        editButton.setVisible(false);
-        editButton.visibleProperty().bind(goatLabel.hoverProperty());
+
+        editButton.visibleProperty().bind(displayView.hoverProperty());
 
         textLabel.textProperty().bindBidirectional(textInput.textProperty());
+        textLabel.setText("some text");
+        textLabel.setStyle("-fx-alignment: bottom-left");
+
+        final FadeTransition fade = new FadeTransition(Duration.millis(400), editButton);
+        fade.setAutoReverse(true);
+        fade.setFromValue(0);
+        fade.setToValue(1);
+
+        displayView.hoverProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                fade.setCycleCount(1);
+                fade.playFromStart();
+            } else {
+                fade.setCycleCount(2);
+                fade.playFrom(Duration.millis(400));
+            }
+        });
+
+        displayView.setSpacing(5);
+        editButton.setStyle("" +
+                "-fx-background-color: transparent;" +
+                "-fx-padding: 3px;" +
+                "-fx-animated: true");
+        editButton.setMinSize(10, 10);
+
 
         editButton.setOnAction(event -> {
-            textLabel.setVisible(!textLabel.isVisible());
-            textInput.setVisible(!textInput.isVisible());
+            displayView.setVisible(false);
+            editView.setVisible(true);
+//            textLabel.setVisible(!textLabel.isVisible());
+//            textInput.setVisible(!textInput.isVisible());
+        });
+
+        doneButton.setOnAction(event -> {
+            displayView.setVisible(true);
+            editView.setVisible(false);
         });
     }
 
