263b4b51ebc85a4949d392d95ea0656b83e30201
Amy Martin <akm96@uclive.ac.nz>
2015-08-06 13:00:14 +1200

    Updated SearchTest to test new command classes. Added comments to new command classes.
    story:281

--- src/main/java/com/thirstygoat/kiqo/command/create/CreateCommand.java
+++ src/main/java/com/thirstygoat/kiqo/command/create/CreateCommand.java
@@ -4,6 +4,10 @@ import com.thirstygoat.kiqo.command.Command;
 import com.thirstygoat.kiqo.search.Searchable;
 import com.thirstygoat.kiqo.search.SearchableItems;
 
+/**
+ * A wrapper to add/remove Searchables to the search index as they are "created" and "uncreated" in the model.
+ * @author amy
+ */
 public abstract class CreateCommand extends Command {
     private Searchable obj;
 
@@ -11,13 +15,19 @@ public abstract class CreateCommand extends Command {
         super();
         this.obj = obj;
     }
-
+    
+    /**
+     * Adds the searchable to the model and the search index.
+     */
     @Override
     public final void execute() {
         addToModel();
         SearchableItems.getInstance().addSearchable(obj);
     }
-
+    
+    /**
+     * Removes the searchable from the search index and the model.
+     */
     @Override
     public final void undo() {
         SearchableItems.getInstance().removeSearchable(obj);
@@ -25,12 +35,12 @@ public abstract class CreateCommand extends Command {
     }
 
     /**
-     * Add the object to the model (used in execute()).
+     * Add the object to the model (used in {@link #execute()}).
      */
     protected abstract void addToModel();
     
     /**
-     * Remove the object from the model (used in undo()).
+     * Remove the object from the model (used in {@link #undo()}).
      */
     protected abstract void removeFromModel();
 }
