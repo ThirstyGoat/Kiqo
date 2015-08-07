87132e33f0f85fe3f997b10112cd67b02437bf13
Sam Schofield <sds53@uclive.ac.nz>
2015-08-07 14:15:47 +1200

    Trying to work out the issues with bindings
    story:288

--- src/main/java/com/thirstygoat/kiqo/gui/nodes/GoatLabelViewModel.java
+++ src/main/java/com/thirstygoat/kiqo/gui/nodes/GoatLabelViewModel.java
@@ -1,6 +1,8 @@
 package com.thirstygoat.kiqo.gui.nodes;
 
 import de.saxsys.mvvmfx.ViewModel;
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ChangeListener;
@@ -10,9 +12,32 @@ import javafx.beans.value.ObservableValue;
  * Created by samschofield on 6/08/15.
  */
 public class GoatLabelViewModel implements ViewModel {
-    private StringProperty textProperty = new SimpleStringProperty("");
+    private StringProperty displayedText = new SimpleStringProperty("");
+    private StringProperty editedTextProperty = new SimpleStringProperty("");
+    private BooleanProperty invalidProperty = new SimpleBooleanProperty(false);
+    public BooleanProperty doneProperty = new SimpleBooleanProperty(false);
 
-    public StringProperty textProperty() {
-        return textProperty;
+    public GoatLabelViewModel() {
+
+    }
+
+    public void setValidation() {
+
+    }
+
+    public BooleanProperty validProperty() {
+        return invalidProperty;
+    }
+
+    public StringProperty displayedTextProperty() {
+        return displayedText;
+    }
+
+    public StringProperty editedTextProperty() {
+        return editedTextProperty;
+    }
+
+    public void setText(String text) {
+        displayedTextProperty().setValue(text);
     }
 }
