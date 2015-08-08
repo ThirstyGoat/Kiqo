12221283ed1acc650cef83ab2b9249fac2efbb3a
Leroy Hopson <git@leroy.geek.nz>
2015-08-09 00:25:37 +1200

    Add additional stringConverters
    Add string converters which accept an ObjectProperty<Organisation> as a
    parameter. This allows a string converters organisation to be set
    dynamically by the view model.
    
    story:288

--- src/main/java/com/thirstygoat/kiqo/util/StringConverters.java
+++ src/main/java/com/thirstygoat/kiqo/util/StringConverters.java
@@ -1,5 +1,6 @@
 package com.thirstygoat.kiqo.util;
 
+import javafx.beans.property.ObjectProperty;
 import javafx.util.StringConverter;
 
 import com.thirstygoat.kiqo.model.Backlog;
@@ -30,6 +31,27 @@ public class StringConverters {
                 for (final Project p : organisation.getProjects()) {
                     if (p.getShortName().equals(shortName)) {
                         return p;
+                    }
+                }
+                return null;
+            }
+        };
+    }
+
+    public static StringConverter<Project> projectStringConverter(ObjectProperty<Organisation> organisationProperty) {
+        return new StringConverter<Project>() {
+            @Override
+            public String toString(Project project) {
+                return project != null ? project.getShortName() : "";
+            }
+
+            @Override
+            public Project fromString(String s) {
+                if (organisationProperty.get() != null) {
+                    for (final Project p : organisationProperty.get().getProjects()) {
+                        if (p.getShortName().equals(s)) {
+                            return p;
+                        }
                     }
                 }
                 return null;
@@ -70,6 +92,27 @@ public class StringConverters {
         };
     }
 
+    public static StringConverter<Person> personStringConverter(ObjectProperty<Organisation> organisationProperty) {
+        return new StringConverter<Person>() {
+            @Override
+            public String toString(Person person) {
+                return person != null ? person.getShortName() : "";
+            }
+
+            @Override
+            public Person fromString(String shortName) {
+                if (organisationProperty.get() != null) {
+                    for (final Person person : organisationProperty.get().getPeople()) {
+                        if (person.getShortName().equals(shortName)) {
+                            return person;
+                        }
+                    }
+                }
+                return null;
+            }
+        };
+    }
+
     public static StringConverter<Scale> scaleStringConverter() {
         return new StringConverter<Scale>() {
             @Override
@@ -103,6 +146,27 @@ public class StringConverters {
         };
     }
 
+    public static StringConverter<Team> teamStringConverter(ObjectProperty<Organisation> organisationProperty) {
+        return new StringConverter<Team>() {
+            @Override
+            public String toString(Team team) {
+                return team != null ? team.getShortName() : "";
+            }
+
+            @Override
+            public Team fromString(String shortName) {
+                if (organisationProperty.get() != null) {
+                    for (final Team team : organisationProperty.get().getTeams()){
+                        if (team.getShortName().equals(shortName)) {
+                            return team;
+                        }
+                    }
+                }
+                return null;
+            }
+        };
+    }
+
     public static StringConverter<Backlog> backlogStringConverter(Organisation organisation) {
         return new StringConverter<Backlog>() {
             @Override
@@ -124,6 +188,27 @@ public class StringConverters {
         };
     }
 
+    public static StringConverter<Backlog> backlogStringConverter(ObjectProperty<Organisation> organisationProperty) {
+        return new StringConverter<Backlog>() {
+            @Override
+            public String toString(Backlog backlog) {
+                return backlog != null ? backlog.getShortName() : "";
+            }
+
+            @Override
+            public Backlog fromString(String shortName) {
+                for (final Project project : organisationProperty.get().getProjects()) {
+                    for (final Backlog backlog : project.getBacklogs()) {
+                        if (backlog.getShortName().equals(shortName)) {
+                            return backlog;
+                        }
+                    }
+                }
+                return null;
+            }
+        };
+    }
+
     public static StringConverter<Release> releaseStringConverter(Organisation organisation) {
         return new StringConverter<Release>() {
             @Override
@@ -134,6 +219,27 @@ public class StringConverters {
             @Override
             public Release fromString(String shortName) {
                 for (final Project project : organisation.getProjects()) {
+                    for (final Release release : project.getReleases()) {
+                        if (release.getShortName().equals(shortName)) {
+                            return release;
+                        }
+                    }
+                }
+                return null;
+            }
+        };
+    }
+
+    public static StringConverter<Release> releaseStringConverter(ObjectProperty<Organisation> organisationProperty) {
+        return new StringConverter<Release>() {
+            @Override
+            public String toString(Release release) {
+                return release != null ? release.getShortName() : "";
+            }
+
+            @Override
+            public Release fromString(String shortName) {
+                for (final Project project : organisationProperty.get().getProjects()) {
                     for (final Release release : project.getReleases()) {
                         if (release.getShortName().equals(shortName)) {
                             return release;
