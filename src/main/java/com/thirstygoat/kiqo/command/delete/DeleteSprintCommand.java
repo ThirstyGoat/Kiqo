8fdadb690d62a488aa3007ce65d5e1bf1e6988fe
Leroy Hopson <git@leroy.geek.nz>
2015-08-09 11:16:52 +1200

    Sprint deletion: Initial implementation
    Create SprintDeleteCommand class.
    Added Sprint deletion to main controller.
    Basic unit testing.
    
    story:288

---
+++ src/main/java/com/thirstygoat/kiqo/command/delete/DeleteSprintCommand.java
@@ -0,0 +1,42 @@
+package com.thirstygoat.kiqo.command.delete;
+
+import com.thirstygoat.kiqo.model.Organisation;
+import com.thirstygoat.kiqo.model.Project;
+import com.thirstygoat.kiqo.model.Sprint;
+
+
+/**
+ * Created by Bradley on 29/04/15.
+ */
+public class DeleteSprintCommand extends DeleteCommand {
+    private final Sprint sprint;
+
+    private int index;
+
+    public DeleteSprintCommand(final Sprint sprint) {
+        super(sprint);
+        this.sprint = sprint;
+    }
+
+    @Override
+    public void removeFromModel() {
+        index = sprint.getRelease().getSprints().indexOf(sprint);
+        sprint.getRelease().getSprints().remove(sprint);
+    }
+
+    
+    @Override
+    public void addToModel() {
+        sprint.getRelease().getSprints().add(index, sprint);
+    }
+
+    @Override
+    public String toString() {
+        return "<Delete Sprint: \"" + sprint.getShortName() + "\">";
+    }
+
+    @Override
+    public String getType() {
+        return "Delete Sprint";
+    }
+}
