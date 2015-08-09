96a9e482dba4ef27ce9686d7a6ff7b3ad098f630
Leroy Hopson <git@leroy.geek.nz>
2015-08-10 11:30:03 +1200

    UpdateListCommand: fix
    Modified so that it keeps track of the items within the new list rather
    than the list itself.

--- src/main/java/com/thirstygoat/kiqo/command/UpdateListCommand.java
+++ src/main/java/com/thirstygoat/kiqo/command/UpdateListCommand.java
@@ -8,14 +8,14 @@ import javafx.collections.ObservableList;
 public final class UpdateListCommand<T> extends Command {
 
     private final List<T> oldContents;
-    private final List<T> newContents;
+    private final List<T> newContents = new ArrayList<>();
     private final ObservableList<T> observableList;
     private final String type;
 
     public UpdateListCommand(String type, List<T> newContents, ObservableList<T> observableList) {
         super();
         this.oldContents = new ArrayList<>(observableList);
-        this.newContents = newContents;
+        this.newContents.addAll(newContents);
         this.observableList = observableList;
         this.type = type;
     }
