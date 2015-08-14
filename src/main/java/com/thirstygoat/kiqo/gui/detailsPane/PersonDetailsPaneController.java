1439fec3b19d9e75694453c527c429a6bc185606
Sam Schofield <sds53@uclive.ac.nz>
2015-08-14 19:36:33 +1200

    started implementing validation for editable labels
    story:288

--- src/main/java/com/thirstygoat/kiqo/gui/detailsPane/PersonDetailsPaneController.java
+++ src/main/java/com/thirstygoat/kiqo/gui/detailsPane/PersonDetailsPaneController.java
@@ -1,36 +1,42 @@
 package com.thirstygoat.kiqo.gui.detailsPane;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
+import com.thirstygoat.kiqo.gui.MainController;
+import com.thirstygoat.kiqo.gui.nodes.GoatLabel;
+import com.thirstygoat.kiqo.gui.nodes.GoatTextArea;
+import com.thirstygoat.kiqo.model.Person;
+import com.thirstygoat.kiqo.util.Utilities;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import org.controlsfx.validation.Severity;
+import org.controlsfx.validation.ValidationSupport;
+import org.controlsfx.validation.Validator;
 
-import com.thirstygoat.kiqo.gui.MainController;
-import com.thirstygoat.kiqo.model.Person;
-import com.thirstygoat.kiqo.util.Utilities;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import java.util.function.Predicate;
 
 /**
  * Created by Carina on 25/03/2015.
  */
 public class PersonDetailsPaneController implements Initializable, IDetailsPaneController<Person> {
     @FXML
-    private Label shortNameLabel;
+    private GoatLabel shortNameLabel;
     @FXML
-    private Label longNameLabel;
+    private GoatLabel longNameLabel;
     @FXML
-    private Label userIdLabel;
+    private GoatLabel userIdLabel;
     @FXML
-    private Label emailLabel;
+    private GoatLabel emailLabel;
     @FXML
-    private Label phoneLabel;
+    private GoatLabel phoneLabel;
     @FXML
-    private Label departmentLabel;
+    private GoatLabel departmentLabel;
     @FXML
     private Label skillsLabel;
     @FXML
-    private Label descriptionLabel;
+    private GoatTextArea descriptionLabel;
 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
@@ -41,13 +47,39 @@ public class PersonDetailsPaneController implements Initializable, IDetailsPaneC
     public void showDetails(final Person person) {
         if (person != null) {
             shortNameLabel.textProperty().bind(person.shortNameProperty());
+            shortNameLabel.setItem(person, "shortName", person.shortNameProperty());
+
             longNameLabel.textProperty().bind(person.longNameProperty());
+            longNameLabel.setItem(person, "longName", person.longNameProperty());
+
             userIdLabel.textProperty().bind(person.userIdProperty());
+            userIdLabel.setItem(person, "userId", person.userIdProperty());
+
             emailLabel.textProperty().bind(person.emailAddressProperty());
+            emailLabel.setItem(person, "emailAddress", person.emailAddressProperty());
+
             phoneLabel.textProperty().bind(person.phoneNumberProperty());
+            phoneLabel.setItem(person, "phoneNumber", person.phoneNumberProperty());
+
             departmentLabel.textProperty().bind(person.departmentProperty());
+            departmentLabel.setItem(person, "department", person.departmentProperty());
+
             descriptionLabel.textProperty().bind(person.descriptionProperty());
+            descriptionLabel.setItem(person, "description", person.descriptionProperty());
+
             skillsLabel.textProperty().bind(Utilities.commaSeparatedValuesProperty(person.observableSkills()));
+
+            final ValidationSupport validationSupport = new ValidationSupport();
+//            final Predicate<String> shortNameValidation = s -> s.length() != 0 &&
+//                    Utilities.shortnameIsUnique(shortNameLabel.getEditField().getText(), person, organisation.getPeople());
+            final Predicate<String> shortNameValidation = s -> s.length() != 0 &&
+                    Utilities.shortnameIsUnique(shortNameLabel.getEditField().getText(), person, new ArrayList<>());
+            //Todo prevent the user from clicking the done button
+            validationSupport.registerValidator(shortNameLabel.getEditField(), Validator.createPredicateValidator(shortNameValidation,
+                    "Short name must be unique and not empty."));
+
+            validationSupport.registerValidator(longNameLabel.getEditField(),
+                    Validator.createEmptyValidator("Name must not be empty", Severity.ERROR));
         } else {
             shortNameLabel.setText(null);
             longNameLabel.setText(null);
