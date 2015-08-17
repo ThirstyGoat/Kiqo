133833d3f4ea62a190c50490b5a221e5b91d3e1a
Leroy Hopson <git@leroy.geek.nz>
2015-08-18 11:23:32 +1200

    Add unit tests for old save files
    Adds a new save file for each version of the product. Attempts to load
    this file and fails unit test is loading raises an exception.

--- src/test/java/com/thirstygoat/kiqo/PersistenceManagerTest.java
+++ src/test/java/com/thirstygoat/kiqo/PersistenceManagerTest.java
@@ -1,9 +1,9 @@
 package com.thirstygoat.kiqo;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-
+import com.google.gson.JsonSyntaxException;
+import com.thirstygoat.kiqo.model.Organisation;
+import com.thirstygoat.kiqo.persistence.PersistenceManager;
+import org.junit.Assert;
 import org.junit.FixMethodOrder;
 import org.junit.Rule;
 import org.junit.Test;
@@ -11,9 +11,9 @@ import org.junit.rules.ExpectedException;
 import org.junit.rules.TemporaryFolder;
 import org.junit.runners.MethodSorters;
 
-import com.google.gson.JsonSyntaxException;
-import com.thirstygoat.kiqo.model.Organisation;
-import com.thirstygoat.kiqo.persistence.PersistenceManager;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
 
 
 // Methods are run in alphabetical order
@@ -55,4 +55,66 @@ public class PersistenceManagerTest {
 
         PersistenceManager.loadOrganisation(f);
     }
+
+    @Test
+    public void testLoad_sprint1File() {
+        final File f = new File(getClass().getResource("/save_files/sprint1.json").getFile());
+        Exception ex = null;
+        try {
+            PersistenceManager.loadOrganisation(f);
+        } catch (Exception e) {
+            ex = e;
+        }
+        Assert.assertTrue("Opening old save file should not raise an exception",
+                ex == null);
+    }
+
+    @Test
+    public void testLoad_sprint2File() {
+        final File f = new File(getClass().getResource("/save_files/sprint2.json").getFile());
+        Exception ex = null;
+        try {
+            PersistenceManager.loadOrganisation(f);
+        } catch (Exception e) {
+            ex = e;
+        }
+        Assert.assertTrue("Opening old save file should not raise an exception",
+                ex == null);
+    }
+
+    @Test
+    public void testLoad_sprint3File() {
+        final File f = new File(getClass().getResource("/save_files/sprint3.json").getFile());
+        Exception ex = null;
+        try {
+            PersistenceManager.loadOrganisation(f);
+        } catch (Exception e) {
+            ex = e;
+        }
+        Assert.assertTrue("Opening old save file should not raise an exception",
+                ex == null);     }
+
+    @Test
+    public void testLoad_sprint4File() {
+        final File f = new File(getClass().getResource("/save_files/sprint4.json").getFile());
+        Exception ex = null;
+        try {
+            PersistenceManager.loadOrganisation(f);
+        } catch (Exception e) {
+            ex = e;
+        }
+        Assert.assertTrue("Opening old save file should not raise an exception",
+                ex == null); }
+
+    @Test
+    public void testLoad_sprint5File() {
+        final File f = new File(getClass().getResource("/save_files/sprint5.json").getFile());
+        Exception ex = null;
+        try {
+            PersistenceManager.loadOrganisation(f);
+        } catch (Exception e) {
+            ex = e;
+        }
+        Assert.assertTrue("Opening old save file should not raise an exception",
+                ex == null); }
 }
