ed36b0d02fd4bbb6c70ed22c5379262c1e1aa619
Leroy Hopson <git@leroy.geek.nz>
2015-09-01 01:58:08 +1200

    updated the pom to use the Thirsty-Goat fork of mvvmFX

--- src/main/java/com/thirstygoat/kiqo/gui/backlog/BacklogViewModel.java
+++ src/main/java/com/thirstygoat/kiqo/gui/backlog/BacklogViewModel.java
@@ -2,16 +2,17 @@ package com.thirstygoat.kiqo.gui.backlog;
 
 import com.thirstygoat.kiqo.command.*;
 import com.thirstygoat.kiqo.command.create.CreateBacklogCommand;
+import com.thirstygoat.kiqo.gui.ModelViewModel;
 import com.thirstygoat.kiqo.model.*;
 import com.thirstygoat.kiqo.util.GoatCollectors;
-import com.thirstygoat.kiqo.util.GoatModelWrapper;
 import com.thirstygoat.kiqo.util.Utilities;
-import de.saxsys.mvvmfx.ViewModel;
-import de.saxsys.mvvmfx.utils.mapping.ModelWrapper;
 import de.saxsys.mvvmfx.utils.validation.*;
 import javafx.beans.binding.Bindings;
 import javafx.beans.binding.BooleanBinding;
-import javafx.beans.property.*;
+import javafx.beans.property.ListProperty;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
@@ -26,11 +27,7 @@ import java.util.stream.Collectors;
  * @author amy
  *
  */
-public class BacklogViewModel implements ViewModel {
-    private final ObjectProperty<Organisation> organisationProperty;
-
-    private ModelWrapper<Backlog> backlogWrapper = new GoatModelWrapper<>();
-
+public class BacklogViewModel extends ModelViewModel<Backlog> {
     private final ListProperty<Story> eligableStories;
 
     private final ObservableRuleBasedValidator shortNameValidator;
@@ -40,8 +37,12 @@ public class BacklogViewModel implements ViewModel {
     private final FunctionBasedValidator<Project> projectValidator;
     private final CompositeValidator allValidator;
 
+    @Override
+    protected Supplier<Backlog> modelSupplier() {
+        return Backlog::new;
+    }
+
     public BacklogViewModel() {
-        organisationProperty = new SimpleObjectProperty<>();
 
         eligableStories = new SimpleListProperty<>(FXCollections.observableArrayList(Item.getWatchStrategy()));
         
@@ -54,7 +55,7 @@ public class BacklogViewModel implements ViewModel {
                     if (projectProperty().get() == null) {
                         return true;
                     }
-                    return Utilities.shortnameIsUnique(shortNameProperty().get(), backlogWrapper.get(),
+                    return Utilities.shortnameIsUnique(shortNameProperty().get(), modelWrapper.get(),
                             projectProperty().get().getBacklogs());
                 },
                 shortNameProperty(), projectProperty());
@@ -75,7 +76,7 @@ public class BacklogViewModel implements ViewModel {
         
         productOwnerValidator = new FunctionBasedValidator<>(productOwnerProperty(),
                 person -> {
-                    return person != null && person.getSkills().contains(organisationProperty.get().getPoSkill());
+                    return person != null && person.getSkills().contains(organisationProperty().get().getPoSkill());
                 },
                 ValidationMessage.error("Product Owner must exist and possess the PO Skill."));
         
@@ -89,42 +90,33 @@ public class BacklogViewModel implements ViewModel {
                 productOwnerValidator, projectValidator);
     }
 
-    public void load(Backlog backlog, Organisation organisation) {
-        organisationProperty().set(organisation);
-
-        if (backlog != null) {
-            backlogWrapper.set(backlog);
-        } else {
-            backlogWrapper.set(new Backlog());
-            backlogWrapper.reset();
-            backlogWrapper.commit();
-        }
-        backlogWrapper.reload();
-
+    @Override
+    protected void afterLoad() {
         eligableStories.setAll(storySupplier().get());
         projectProperty().addListener(change -> {
             eligableStories.setAll(storySupplier().get());
         });
     }
 
-    public void reset() {
-        backlogWrapper.reset();
-    }
-
-    public void reload() {
-        backlogWrapper.reload();
-    }
-
+    /**
+     * @return A list of valid projects that this backlog can belong to.
+     */
     protected Supplier<List<Project>> projectSupplier() {
         return () -> organisationProperty().get().getProjects();
     }
 
+    /**
+     * @return A collection of valid product owners that can own this backlog.
+     */
     protected Supplier<List<Person>> productOwnerSupplier() {
         return () -> organisationProperty().get().getPeople().stream()
                 .filter(p -> p.getSkills().contains((organisationProperty().get().getPoSkill())))
                 .collect(Collectors.toList());
     }
 
+    /**
+     * @return A collections of valid stories that can belong to this backlog.
+     */
     protected Supplier<ObservableList<Story>> storySupplier() {
         return () -> {
             if (projectProperty().get() == null) {
@@ -137,51 +129,52 @@ public class BacklogViewModel implements ViewModel {
         };
     }
 
+    @Override
     public Command getCommand() {
         final Command command;
         if (!allValidation().isValid()) {
             // Properties are not valid
             return null;
         }
-        if (!backlogWrapper.isDifferent() && !stories().containsAll(backlogWrapper.get().getStories())
-                && !backlogWrapper.get().getStories().containsAll(stories())) {
+        if (!modelWrapper.isDifferent() && !stories().containsAll(modelWrapper.get().getStories())
+                && !modelWrapper.get().getStories().containsAll(stories())) {
             // Nothing changed
             return null;
         }
         final ArrayList<Story> stories = new ArrayList<>();
         stories.addAll(stories());
 
-        if (backlogWrapper.get().getShortName() == "") { // Must be a new backlog
+        if (modelWrapper.get().getShortName() == "") { // Must be a new backlog
             final Backlog backlog = new Backlog(shortNameProperty().get(), longNameProperty().get(),
                     descriptionProperty().get(), productOwnerProperty().get(), projectProperty().get(), stories(),
                     scaleProperty().get());
             command = new CreateBacklogCommand(backlog);
         } else {
             final ArrayList<Command> changes = new ArrayList<>();
-            if (!longNameProperty().getValue().equals(backlogWrapper.get().getLongName())) {
-                changes.add(new EditCommand<>(backlogWrapper.get(), "longName", longNameProperty().getValue()));
+            if (!longNameProperty().getValue().equals(modelWrapper.get().getLongName())) {
+                changes.add(new EditCommand<>(modelWrapper.get(), "longName", longNameProperty().getValue()));
             }
-            if (!shortNameProperty().getValue().equals(backlogWrapper.get().getShortName())) {
-                changes.add(new EditCommand<>(backlogWrapper.get(), "shortName", shortNameProperty().getValue()));
+            if (!shortNameProperty().getValue().equals(modelWrapper.get().getShortName())) {
+                changes.add(new EditCommand<>(modelWrapper.get(), "shortName", shortNameProperty().getValue()));
             }
-            if (!descriptionProperty().getValue().equals(backlogWrapper.get().getDescription())) {
-                changes.add(new EditCommand<>(backlogWrapper.get(), "description", descriptionProperty().getValue()));
-            }
-
-            if (!productOwnerProperty().equals(backlogWrapper.get().getProductOwner())) {
-                changes.add(new EditCommand<>(backlogWrapper.get(), "productOwner", productOwnerProperty().get()));
+            if (!descriptionProperty().getValue().equals(modelWrapper.get().getDescription())) {
+                changes.add(new EditCommand<>(modelWrapper.get(), "description", descriptionProperty().getValue()));
             }
 
-            if (scaleProperty().getValue() != backlogWrapper.get().getScale()) {
-                changes.add(new EditCommand<>(backlogWrapper.get(), "scale", scaleProperty().getValue()));
+            if (!productOwnerProperty().equals(modelWrapper.get().getProductOwner())) {
+                changes.add(new EditCommand<>(modelWrapper.get(), "productOwner", productOwnerProperty().get()));
+            }
+
+            if (scaleProperty().getValue() != modelWrapper.get().getScale()) {
+                changes.add(new EditCommand<>(modelWrapper.get(), "scale", scaleProperty().getValue()));
             }
 
             // Stories being added to the backlog
             final ArrayList<Story> addedStories = new ArrayList<>(stories());
-            addedStories.removeAll(backlogWrapper.get().getStories());
+            addedStories.removeAll(modelWrapper.get().getStories());
 
             // Stories being removed from the backlog
-            final ArrayList<Story> removedStories = new ArrayList<>(backlogWrapper.get().getStories());
+            final ArrayList<Story> removedStories = new ArrayList<>(modelWrapper.get().getStories());
             removedStories.removeAll(stories());
 
             for (Story story : addedStories) {
@@ -190,67 +183,63 @@ public class BacklogViewModel implements ViewModel {
                     changes.add(new EditCommand<>(story, "scale", scaleProperty().get()));
                 }
                 changes.add(new MoveItemCommand<>(story, projectProperty().get().observableUnallocatedStories(),
-                        backlogWrapper.get().observableStories()));
-                changes.add(new EditCommand<>(story, "backlog", backlogWrapper.get()));
+                        modelWrapper.get().observableStories()));
+                changes.add(new EditCommand<>(story, "backlog", modelWrapper.get()));
             }
             // get the remaining stories and change their scales - might be a better way to do this rather than 2 loops
-            for (Story story : backlogWrapper.get().getStories()) {
+            for (Story story : modelWrapper.get().getStories()) {
                 if (story.getScale() != scaleProperty().get()) {
                     changes.add(new EditCommand<>(story, "estimate", 0));
                     changes.add(new EditCommand<>(story, "scale", scaleProperty().get()));
                 }
             }
 
-            if (projectProperty().get().getShortName() != "" && !projectProperty().get().equals(backlogWrapper.get().getProject())) {
-                changes.add(new MoveItemCommand<>(backlogWrapper.get(), backlogWrapper.get().getProject().observableBacklogs(),
+            if (projectProperty().get().getShortName() != "" && !projectProperty().get().equals(modelWrapper.get().getProject())) {
+                changes.add(new MoveItemCommand<>(modelWrapper.get(), modelWrapper.get().getProject().observableBacklogs(),
                         projectProperty().get().observableBacklogs()));
-                changes.add(new EditCommand<>(backlogWrapper.get(), "project", projectProperty().get()));
+                changes.add(new EditCommand<>(modelWrapper.get(), "project", projectProperty().get()));
                 // If backlog moved to a different project we need to update the back references of the stories
                 // in that backlog.
-                for (Story story : backlogWrapper.get().observableStories()) {
+                for (Story story : modelWrapper.get().observableStories()) {
                     changes.add(new EditCommand<>(story, "project", projectProperty().get()));
-                    changes.add(new EditCommand<>(story, "backlog", backlogWrapper.get()));
+                    changes.add(new EditCommand<>(story, "backlog", modelWrapper.get()));
                 }
             }
 
             for (Story story : removedStories) {
-                changes.add(new RemoveStoryFromBacklogCommand(story, backlogWrapper.get()));
+                changes.add(new RemoveStoryFromBacklogCommand(story, modelWrapper.get()));
             }
             command = new CompoundCommand("Edit Backlog", changes);
         }
         return command;
     }
 
-    public ObjectProperty<Organisation> organisationProperty() {
-        return organisationProperty;
-    }
-
     public StringProperty shortNameProperty() {
-        return backlogWrapper.field("shortName", Backlog::getShortName, Backlog::setShortName, "");
+        return modelWrapper.field("shortName", Backlog::getShortName, Backlog::setShortName, "");
     }
 
     public StringProperty longNameProperty() {
-        return backlogWrapper.field("longName", Backlog::getLongName, Backlog::setLongName, "");
+        return modelWrapper.field("longName", Backlog::getLongName, Backlog::setLongName, "");
     }
 
     public StringProperty descriptionProperty() {
-        return backlogWrapper.field("description", Backlog::getDescription, Backlog::setDescription, "");
+        return modelWrapper.field("description", Backlog::getDescription, Backlog::setDescription, "");
     }
 
     public ObjectProperty<Person> productOwnerProperty() {
-        return backlogWrapper.field("productOwner", Backlog::getProductOwner, Backlog::setProductOwner, new Person());
+        return modelWrapper.field("productOwner", Backlog::getProductOwner, Backlog::setProductOwner, new Person());
     }
 
     public ObjectProperty<Project> projectProperty() {
-        return backlogWrapper.field("project", Backlog::getProject, Backlog::setProject, new Project());
+        return modelWrapper.field("project", Backlog::getProject, Backlog::setProject, new Project());
     }
 
     public ObjectProperty<Scale> scaleProperty() {
-        return backlogWrapper.field("backlog", Backlog::getScale, Backlog::setScale, Scale.FIBONACCI);
+        return modelWrapper.field("backlog", Backlog::getScale, Backlog::setScale, Scale.FIBONACCI);
     }
 
     public ListProperty<Story> stories() {
-        return backlogWrapper.field("stories", Backlog::getStories, Backlog::setStories, Collections.emptyList()); 
+        return modelWrapper.field("stories", Backlog::getStories, Backlog::setStories, Collections.emptyList());
     }
 
     public ListProperty<Story> eligableStories() {
