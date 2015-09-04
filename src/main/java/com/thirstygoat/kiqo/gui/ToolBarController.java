7d83f633a5a243b0548a37012a9afea977ebce11
carina <cdb71@uclive.ac.nz>
2015-09-04 17:10:28 +1200

    Adding creation stuff to toolbar
    Implemented the popOver to create new items via the toolbar, added the button.
    Set up the buttons that will be in the popOver.
    
     story:355

--- src/main/java/com/thirstygoat/kiqo/gui/ToolBarController.java
+++ src/main/java/com/thirstygoat/kiqo/gui/ToolBarController.java
@@ -1,15 +1,16 @@
 package com.thirstygoat.kiqo.gui;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
+import com.thirstygoat.kiqo.command.UndoManager;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ToolBar;
+import javafx.scene.layout.GridPane;
+import org.controlsfx.control.PopOver;
 
-import com.thirstygoat.kiqo.command.UndoManager;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 
 public class ToolBarController implements Initializable {
@@ -18,6 +19,8 @@ public class ToolBarController implements Initializable {
     private Button openButton;
     @FXML
     private Button saveButton;
+    @FXML
+    private Button newButton;
     @FXML
     private Button undoButton;
     @FXML
@@ -29,10 +32,48 @@ public class ToolBarController implements Initializable {
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
+        initializeButtons();
         addMenuItemHandlers();
         setListenersOnUndoManager();
+
     }
 
+    private void initializeButtons() {
+        Button projectButton = new Button();
+        projectButton.setOnAction(event -> {
+            mainController.newProject();
+        });
+        Button personButton = new Button();
+        personButton.setOnAction(event -> {
+            mainController.newPerson();
+        });
+        Button skillButton = new Button();
+        skillButton.setOnAction(event -> {
+            mainController.newSkill();
+        });
+        Button teamButton = new Button();
+        teamButton.setOnAction(event -> {
+            mainController.newTeam();
+        });
+        Button releaseButton = new Button();
+        releaseButton.setOnAction(event -> {
+            mainController.newRelease();
+        });
+        Button storyButton = new Button();
+        storyButton.setOnAction(event -> {
+            mainController.newStory();
+        });
+        Button backlogButton = new Button();
+        backlogButton.setOnAction(event -> {
+            mainController.newBacklog();
+        });
+        Button sprintButton = new Button();
+        sprintButton.setOnAction(event -> {
+            mainController.newSprint();
+        });
+
+
+    }
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
@@ -41,6 +82,17 @@ public class ToolBarController implements Initializable {
     private void addMenuItemHandlers() {
         undoButton.setOnAction(event -> mainController.undo());
         redoButton.setOnAction(event -> mainController.redo());
+
+        PopOver newItemPopOver = new PopOver();
+        GridPane gridpane = new GridPane();
+        newItemPopOver.setContentNode((new GridPane()));
+        newItemPopOver.setDetachable(false);
+        newButton.setOnAction(event -> newItemPopOver.show(newButton));
+        newItemPopOver.focusedProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue) {
+                newItemPopOver.hide();
+            }
+        });
 
         reportButton.setOnAction(event -> mainController.statusReport());
         openButton.setOnAction(event -> mainController.openOrganisation(null));
