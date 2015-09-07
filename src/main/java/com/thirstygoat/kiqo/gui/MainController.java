b0ea376f801bec190a84ea5735b1544c86c0bdbe
jjharrison-uc <jjh72@uclive.ac.nz>
2015-09-08 09:24:29 +1200

    Change was lost in the merge
    story:284

--- src/main/java/com/thirstygoat/kiqo/gui/MainController.java
+++ src/main/java/com/thirstygoat/kiqo/gui/MainController.java
@@ -915,21 +915,6 @@ public class MainController implements Initializable {
                 viewTuple.getCodeBehind().setExitStrategy(() -> stage.close());
                 stage.setScene(new Scene(viewTuple.getView()));
                 stage.showAndWait();
-            } else if (type.equals("TaskCard")) {
-                System.out.println("TaskCard selected?!?");
-                ViewTuple<TaskCardExpandedView, TaskCardViewModel> taskFormTuple = FluentViewLoader.fxmlView(TaskCardExpandedView.class).load();
-                // viewModel
-                final TaskCardViewModel viewModel = taskFormTuple.getViewModel();
-
-                Task task = selectedOrganisationProperty.get().getProjects().get(0).getBacklogs().get(0).getStories().get(0).getTasks().get(1);
-
-//                viewModel.load(task, selectedOrganisationProperty.get());
-                // view
-                viewModel.setExitStrategy(() -> stage.close());
-                stage.setScene(new Scene(taskFormTuple.getView()));
-                System.out.println("mc: scene has been set");
-                viewModel.load(task, selectedOrganisationProperty.get());
-                stage.showAndWait();
             } else {
                 final FXMLLoader loader = new FXMLLoader();
                 loader.setLocation(MainController.class.getClassLoader().getResource("forms/" + type.toLowerCase() + ".fxml"));
