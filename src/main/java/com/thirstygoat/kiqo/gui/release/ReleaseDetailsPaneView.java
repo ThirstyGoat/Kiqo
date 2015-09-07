ca51721df774471514530c1ea292d077bfd9d95b
Amy Martin <akm96@uclive.ac.nz>
2015-09-07 14:02:59 +1200

    Replaced labels with GoatLabels in Release DetailsPane (still waiting for the DatePicker and FilteredListSelection goat labels).
    story:355

--- src/main/java/com/thirstygoat/kiqo/gui/release/ReleaseDetailsPaneView.java
+++ src/main/java/com/thirstygoat/kiqo/gui/release/ReleaseDetailsPaneView.java
@@ -5,26 +5,30 @@ import java.util.ResourceBundle;
 
 import javafx.fxml.*;
 import javafx.scene.control.Label;
+
+import com.thirstygoat.kiqo.gui.nodes.*;
+import com.thirstygoat.kiqo.util.FxUtils;
+
 import de.saxsys.mvvmfx.*;
 
 public class ReleaseDetailsPaneView implements FxmlView<ReleaseDetailsPaneViewModel>, Initializable {
     @FXML
-    private Label shortNameLabel;
+    private GoatLabelTextField shortNameLabel;
     @FXML
     private Label projectLabel;
     @FXML
     private Label releaseDateLabel;
     @FXML
-    private Label descriptionLabel;
+    private GoatLabelTextArea descriptionLabel;
 
     @InjectViewModel
     private ReleaseDetailsPaneViewModel viewModel;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        shortNameLabel.textProperty().bind(viewModel.shortNameProperty());
+        FxUtils.initGoatLabel(shortNameLabel, viewModel, viewModel.shortNameProperty(), viewModel.shortNameValidation());
         projectLabel.textProperty().bind(viewModel.projectNameProperty());
         releaseDateLabel.textProperty().bind(viewModel.dateStringProperty());
-        descriptionLabel.textProperty().bind(viewModel.descriptionProperty());
+        FxUtils.initGoatLabel(descriptionLabel, viewModel, viewModel.descriptionProperty(), viewModel.descriptionValidation(), "Description");
     }
 }
