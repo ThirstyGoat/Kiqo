79ce66102744ab4148b0c8989a1b2e4838a04c2d
Sam Schofield <sds53@uclive.ac.nz>
2015-09-11 02:59:38 +1200

    Initial implementation of GoatLabelFilteredListSelectionView
    Changed GoatFilteredListSelectionView to extend control instead of
    vbox so that it can be used with editable goat labels. + Implemented skin
    
    Added defaultText string property + getters and setters so that the
    default text for a goat label can be set from the fxml
    
    Added and FXUtil method to populate FilteredListSelectionView with items
    
    story:355

--- src/main/java/com/thirstygoat/kiqo/util/FxUtils.java
+++ src/main/java/com/thirstygoat/kiqo/util/FxUtils.java
@@ -4,10 +4,13 @@ import com.thirstygoat.kiqo.gui.Editable;
 import com.thirstygoat.kiqo.gui.nodes.*;
 import com.thirstygoat.kiqo.model.Item;
 import de.saxsys.mvvmfx.utils.validation.ValidationStatus;
+import javafx.beans.binding.Bindings;
 import javafx.beans.property.FloatProperty;
+import javafx.beans.property.ListProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.StringProperty;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputControl;
@@ -17,8 +20,6 @@ import javafx.util.converter.NumberStringConverter;
 import org.controlsfx.control.textfield.AutoCompletionBinding;
 import org.controlsfx.control.textfield.TextFields;
 
-import java.text.NumberFormat;
-import java.text.ParseException;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
@@ -27,11 +28,11 @@ import java.util.stream.Collectors;
 
 public final class FxUtils {
     public static <E extends Item> void setTextFieldSuggester(TextField textField, Supplier<List<E>> listSupplier) {
-        
+
         // use a callback to get an up-to-date list, instead of just whatever exists at initialisation.
         // use a String converter so that the short name is used.
         final AutoCompletionBinding<E> binding = TextFields.bindAutoCompletion(textField, new Callback<AutoCompletionBinding.ISuggestionRequest, Collection<E>>() {
-            
+
             @Override
             public Collection<E> call(AutoCompletionBinding.ISuggestionRequest request) {
                 List<E> list = listSupplier.get();
@@ -217,5 +218,13 @@ public final class FxUtils {
         goatLabel.displayTextProperty().bindBidirectional(stringProperty);
         goatLabel.getEditField().valueProperty().bindBidirectional(objectProperty);
         goatLabel.validationStatus().set(validationStatus);
+    }
+
+    public static <T extends Item> void initGoatLabel(GoatLabelFilteredListSelectionView<T> goatLabel,
+                                                      Editable viewModel, ListProperty<T> targetList,
+                                                      ObservableList<T> sourceList, Object o) {
+        initGoatLabelActions(goatLabel, viewModel);
+        goatLabel.displayTextProperty().bind(Bindings.concat(targetList));
+        goatLabel.getEditField().setSourceItems(sourceList);
     }
 }
