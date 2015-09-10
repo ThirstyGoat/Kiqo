56b1f860f9c2b6b52dc072234fce2631122bd77d
Leroy Hopson <git@leroy.geek.nz>
2015-09-11 00:58:26 +1200

    Die tabs, die!
    story:355

--- src/main/java/com/thirstygoat/kiqo/gui/DragContainer.java
+++ src/main/java/com/thirstygoat/kiqo/gui/DragContainer.java
@@ -10,28 +10,28 @@ import javafx.util.Pair;
 import com.thirstygoat.kiqo.model.AcceptanceCriteria;
 
 public class DragContainer implements Serializable {
-	public static final DataFormat DATA_FORMAT = new DataFormat(AcceptanceCriteria.class.getPackage().getName() + ".dataformat");
-	private static final long serialVersionUID = -1890998765646621338L;
-	private final List <Pair<String, Object> > dataPairs = new ArrayList <Pair<String, Object> > ();
-	
-	public void addData (String key, Object value) {
-		dataPairs.add(new Pair<String, Object>(key, value));		
-	}
-	
-	@SuppressWarnings("unchecked")
+    public static final DataFormat DATA_FORMAT = new DataFormat(AcceptanceCriteria.class.getPackage().getName() + ".dataformat");
+    private static final long serialVersionUID = -1890998765646621338L;
+    private final List <Pair<String, Object> > dataPairs = new ArrayList <Pair<String, Object> > ();
+
+    public void addData (String key, Object value) {
+        dataPairs.add(new Pair<String, Object>(key, value));
+    }
+
+    @SuppressWarnings("unchecked")
     public <T> T getValue (String key) {
-		
-		for (Pair<String, Object> data: dataPairs) {
-			
-			if (data.getKey().equals(key))
-				return (T) data.getValue();
-				
-		}
-		
-		return null;
-	}
-	
-	public List <Pair<String, Object> > getData () { 
-	    return dataPairs; 
-	}	
+
+        for (Pair<String, Object> data: dataPairs) {
+
+            if (data.getKey().equals(key))
+                return (T) data.getValue();
+
+        }
+
+        return null;
+    }
+
+    public List <Pair<String, Object> > getData () {
+        return dataPairs;
+    }
 }
