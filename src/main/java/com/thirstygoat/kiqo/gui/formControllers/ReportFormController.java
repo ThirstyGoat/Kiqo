6ed87f4f0f7938d84993684415806d6ae9c001f2
Sam Schofield <sds53@uclive.ac.nz>
2015-09-17 01:04:08 +1200

    updated report gen to use goatFilteredListSelectionView instead of GoatListSelectionView story:285

--- src/main/java/com/thirstygoat/kiqo/gui/formControllers/ReportFormController.java
+++ src/main/java/com/thirstygoat/kiqo/gui/formControllers/ReportFormController.java
@@ -1,31 +1,26 @@
 package com.thirstygoat.kiqo.gui.formControllers;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
+import com.thirstygoat.kiqo.gui.nodes.GoatFilteredListSelectionView;
+import com.thirstygoat.kiqo.model.Item;
+import com.thirstygoat.kiqo.model.Organisation;
+import com.thirstygoat.kiqo.model.Project;
 import javafx.application.Platform;
 import javafx.beans.binding.Bindings;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListCell;
 import javafx.stage.Stage;
 
-import com.thirstygoat.kiqo.gui.nodes.GoatListSelectionView;
-import com.thirstygoat.kiqo.model.Item;
-import com.thirstygoat.kiqo.model.Organisation;
-import com.thirstygoat.kiqo.model.Project;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * Created by Carina and James on 27/05/15.
  */
 public class ReportFormController implements Initializable {
-    private final ObservableList<Item> targetList = FXCollections.observableArrayList();
     private Stage stage;
     private Organisation organisation;
     private boolean valid = false;
@@ -35,7 +30,7 @@ public class ReportFormController implements Initializable {
     @FXML
     private ComboBox<String> levelComboBox;
     @FXML
-    private GoatListSelectionView<Item> elementListSelectionView;
+    private GoatFilteredListSelectionView<Item> elementListSelectionView;
     @FXML
     private Button okButton;
     @FXML
@@ -45,13 +40,11 @@ public class ReportFormController implements Initializable {
         setButtonHandlers();
         populateComboBox();
         setListeners();
-        setListSelectionViewSettings();
-
         Platform.runLater(levelComboBox::requestFocus);
     }
 
     public ObservableList<Item> getTargetList() {
-        return targetList;
+        return elementListSelectionView.getTargetItems();
     }
 
     private void setButtonHandlers() {
@@ -64,7 +57,7 @@ public class ReportFormController implements Initializable {
 
     private void setListeners() {
         levelComboBox.valueProperty().addListener(((observable, oldValue, newValue) -> {
-            elementListSelectionView.getTargetListView().getItems().clear();
+            elementListSelectionView.targetItemsProperty().clear();
             setListSelectionViewData(newValue);
             if (newValue.equals("Organisation")) {
                 elementListSelectionView.setDisable(true);
@@ -73,7 +66,7 @@ public class ReportFormController implements Initializable {
             }
         }));
 
-        okButton.disableProperty().bind(Bindings.isEmpty(targetList)
+        okButton.disableProperty().bind(Bindings.isEmpty(elementListSelectionView.getTargetItems())
                 .and(Bindings.notEqual(levelComboBox.valueProperty(), "Organisation")));
     }
 
@@ -96,29 +89,12 @@ public class ReportFormController implements Initializable {
                 sourceList.addAll(project.getBacklogs());
             }
         }
-        elementListSelectionView.setSourceHeader(new Label("Available " + newValue + ":"));
-        elementListSelectionView.setTargetHeader(new Label(newValue + " in the report:"));
-        elementListSelectionView.getSourceListView().setItems(sourceList);
-        elementListSelectionView.getTargetListView().setItems(targetList);
+        elementListSelectionView.getSourceItems().setAll(sourceList);
     }
 
     private void populateComboBox() {
         levelComboBox.getItems().addAll("Organisation", "Projects", "Teams", "People", "Backlogs");
         levelComboBox.getSelectionModel().select(0);
-    }
-
-    private void setListSelectionViewSettings() {
-        elementListSelectionView.setPadding(new Insets(0, 0, 0, 0));
-
-        elementListSelectionView.setCellFactories(view -> {
-            final ListCell<Item> cell = new ListCell<Item>() {
-                public void updateItem(Item item, boolean empty) {
-                    super.updateItem(item, empty);
-                    setText(item != null ? item.getShortName() : null);
-                }
-            };
-            return cell;
-        });
     }
 
     public boolean isValid() {
