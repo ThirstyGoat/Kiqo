9a3b2f8fa7b2fa70487ffb173f2a9b973ec341f4
Amy Martin <akm96@uclive.ac.nz>
2015-09-18 21:38:50 +1200

    Refactored the custom cell for filteredlistbicontrol in the teamdetailspane.
    story:355

--- src/main/java/com/thirstygoat/kiqo/gui/team/PersonListItemViewModel.java
+++ src/main/java/com/thirstygoat/kiqo/gui/team/PersonListItemViewModel.java
@@ -1,44 +1,60 @@
 package com.thirstygoat.kiqo.gui.team;
 
+import javafx.beans.binding.*;
 import javafx.beans.property.*;
+import javafx.scene.paint.Color;
 
 import com.thirstygoat.kiqo.gui.person.PersonViewModel;
 import com.thirstygoat.kiqo.model.Person;
 
 public class PersonListItemViewModel extends PersonViewModel {
     public static enum Role {
-        PRODUCT_OWNER("Product Owner", "blue"),
-        SCRUM_MASTER("Scrum Master", "red"),
-        DEVELOPMENT("Development", "green"),
-        OTHER("Other", "grey");
+        PRODUCT_OWNER("Product Owner", Color.BLUE),
+        SCRUM_MASTER("Scrum Master", Color.RED),
+        DEVELOPMENT("Development", Color.GREEN),
+        OTHER("Other", Color.GREY);
         
-        private String name, color;
+        private String name;
+        private Color color;
         
-        private Role(String name, String color) {
+        private Role(String name, Color color) {
             this.name = name;
             this.color = color;
         }
         
-        public String getName() {
+        @Override
+        public String toString() {
             return name;
         }
         
-        public String getStyle() {
-            return "-fx-text-fill: " + color + ";";
+        private Color getColor() {
+            return color;
         }
     }
     
     private ObjectProperty<Role> role;
+    private ObjectBinding<Color> roleColor;
     
     public PersonListItemViewModel() {
         role = new SimpleObjectProperty<>(Role.OTHER);
+        roleColor = Bindings.createObjectBinding(
+                () -> { return roleProperty().get().getColor(); }, 
+                roleProperty());
     }
 
     protected ObjectProperty<Role> roleProperty() {
         return role;
     }
+    
+    protected ObjectBinding<Color> roleColorBinding() {
+        return roleColor;
+    }
 
     public Person getPerson() {
         return modelWrapper.get();
+    }
+
+    public static String getColorString(Role role) {
+        return role.getColor().toString();
     }
 }
