58fd36e6f13140c015a025e8f118a6fa0e9423f9
Sam Schofield <sds53@uclive.ac.nz>
2015-09-20 00:00:24 +1200

    Getting xAxis range to update when the sprint start / end dates change
    story:286

--- src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurnDownView.java
+++ src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurnDownView.java
@@ -2,9 +2,12 @@ package com.thirstygoat.kiqo.gui.sprint;
 
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.beans.binding.Bindings;
+import javafx.beans.binding.LongBinding;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 
@@ -25,23 +28,36 @@ public class SprintDetailsPaneBurnDownView implements FxmlView<SprintDetailsPane
     private Button button;
 
     private XYChart.Series<Number, Number> targetLineSeries = new XYChart.Series<>();
+    private XYChart.Series<Number, Number> loggedHoursSeries = new XYChart.Series<>();
+    private XYChart.Series<Number, Number> burndownSeries = new XYChart.Series<>();
+
     private static int sum = 0;
+    private static int day = 0;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
 
+        //TODO change X axis so that it shows dates
+        burndownChart.getXAxis().setAutoRanging(false);
         viewModel.sprintProperty().addListener((observable, oldValue, newValue) -> {
-            System.out.println(viewModel.sprintProperty().getValue().startDateProperty().getValue().getDayOfMonth());
+            LongBinding dateRangeBinding = Bindings.createLongBinding(() ->
+                    viewModel.sprintProperty().get().endDateProperty().get().toEpochDay() -
+                            viewModel.sprintProperty().get().startDateProperty().get().toEpochDay()
+                    , viewModel.sprintProperty().get().startDateProperty(), viewModel.sprintProperty().get().endDateProperty());
 
+            targetLineSeries.getData().add(new XYChart.Data<>(0, 100));
+            targetLineSeries.getData().add(new XYChart.Data<>(viewModel.sprintProperty().get().endDateProperty().get().toEpochDay() - viewModel.sprintProperty().get().startDateProperty().get().toEpochDay(), 0));
+            ((NumberAxis) burndownChart.getXAxis()).upperBoundProperty().bind(dateRangeBinding);
         });
 
+        burndownChart.getData().addAll(targetLineSeries, loggedHoursSeries, burndownSeries);
+
+        //TODO remove (for testing purposes only
         button.setOnAction(event -> {
-            targetLineSeries.getData().add(new XYChart.Data<>(sum, 10));
+            loggedHoursSeries.getData().add(new XYChart.Data<>(day, sum));
             sum += 10;
+            day += 1;
         });
-
-        burndownChart.getXAxis().setAutoRanging(true);
-        burndownChart.getData().add(targetLineSeries);
     }
 
     public SprintDetailsPaneBurndownViewModel getViewModel() {
--- src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurndownViewModel.java
+++ src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurndownViewModel.java
@@ -1,27 +1,15 @@
 package com.thirstygoat.kiqo.gui.sprint;
 
 import com.thirstygoat.kiqo.gui.Editable;
-import com.thirstygoat.kiqo.model.Sprint;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
+
 import javafx.scene.chart.XYChart;
 
 
 
 public class SprintDetailsPaneBurndownViewModel extends SprintViewModel implements Editable {
 
-    private ObjectProperty<XYChart.Series<Number, Number>> targetLineSeriesProperty = new SimpleObjectProperty<>();
-    private static int sum = 0;
-
     public SprintDetailsPaneBurndownViewModel() {
         super();
-    }
-
-    public void addData() {
-        targetLineSeriesProperty.get().getData().add(new XYChart.Data<>(sum, 10));
-        sum += 10;
     }
 
     @Override
