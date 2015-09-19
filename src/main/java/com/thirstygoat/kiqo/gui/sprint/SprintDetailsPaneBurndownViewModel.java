75aeff27ea6ca108d698394fe536a07ee84d92b3
Sam Schofield <sds53@uclive.ac.nz>
2015-09-19 21:21:15 +1200

    Added burndown viewmodel
    story:286

---
+++ src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurndownViewModel.java
@@ -0,0 +1,28 @@
+package com.thirstygoat.kiqo.gui.sprint;
+
+import com.thirstygoat.kiqo.command.Command;
+import com.thirstygoat.kiqo.command.UndoManager;
+import com.thirstygoat.kiqo.gui.Editable;
+import com.thirstygoat.kiqo.util.Utilities;
+import javafx.beans.binding.Bindings;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * Created by Carina Blair on 5/08/2015.
+ */
+public class SprintDetailsPaneBurndownViewModel extends SprintViewModel implements Editable {
+
+
+    @Override
+    public void commitEdit() {
+
+    }
+
+    @Override
+    public void cancelEdit() {
+
+    }
+}
+
+
