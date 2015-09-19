75b35433049190257c3a2ef63194d2b8e1f23253
Sam Schofield <sds53@uclive.ac.nz>
2015-09-20 10:30:16 +1200

    Implemented data bindings from view to viewModel so all the data manipulation can be done in the vm without touching the view
    story:286

--- src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurnDownView.java
+++ src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurnDownView.java
@@ -2,12 +2,9 @@ package com.thirstygoat.kiqo.gui.sprint;
 
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
-import javafx.beans.binding.Bindings;
-import javafx.beans.binding.LongBinding;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
-import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 
@@ -31,33 +28,16 @@ public class SprintDetailsPaneBurnDownView implements FxmlView<SprintDetailsPane
     private XYChart.Series<Number, Number> loggedHoursSeries = new XYChart.Series<>();
     private XYChart.Series<Number, Number> burndownSeries = new XYChart.Series<>();
 
-    private static int sum = 0;
-    private static int day = 0;
-
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-
-        //TODO change X axis so that it shows dates
-        burndownChart.getXAxis().setAutoRanging(false);
-        viewModel.sprintProperty().addListener((observable, oldValue, newValue) -> {
-            LongBinding dateRangeBinding = Bindings.createLongBinding(() ->
-                    viewModel.sprintProperty().get().endDateProperty().get().toEpochDay() -
-                            viewModel.sprintProperty().get().startDateProperty().get().toEpochDay()
-                    , viewModel.sprintProperty().get().startDateProperty(), viewModel.sprintProperty().get().endDateProperty());
-
-            targetLineSeries.getData().add(new XYChart.Data<>(0, 100));
-            targetLineSeries.getData().add(new XYChart.Data<>(viewModel.sprintProperty().get().endDateProperty().get().toEpochDay() - viewModel.sprintProperty().get().startDateProperty().get().toEpochDay(), 0));
-            ((NumberAxis) burndownChart.getXAxis()).upperBoundProperty().bind(dateRangeBinding);
-        });
+        targetLineSeries.dataProperty().bind(viewModel.targetLineDataProperty());
+        loggedHoursSeries.dataProperty().bind(viewModel.loggedHoursDataProperty());
+        burndownSeries.dataProperty().bind(viewModel.burndownDataProperty());
 
         burndownChart.getData().addAll(targetLineSeries, loggedHoursSeries, burndownSeries);
 
-        //TODO remove (for testing purposes only
-        button.setOnAction(event -> {
-            loggedHoursSeries.getData().add(new XYChart.Data<>(day, sum));
-            sum += 10;
-            day += 1;
-        });
+        //TODO remove this
+        button.setOnAction(event -> viewModel.buttonPress());
     }
 
     public SprintDetailsPaneBurndownViewModel getViewModel() {
--- src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurndownViewModel.java
+++ src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurndownViewModel.java
@@ -1,25 +1,52 @@
 package com.thirstygoat.kiqo.gui.sprint;
 
 import com.thirstygoat.kiqo.gui.Editable;
-
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.chart.XYChart;
 
 
 
 public class SprintDetailsPaneBurndownViewModel extends SprintViewModel implements Editable {
+    private ObservableList<XYChart.Data<Number, Number>> targetLineData = FXCollections.observableArrayList();
+    private ObjectProperty<ObservableList<XYChart.Data<Number, Number>>> targetLineDataProperty = new SimpleObjectProperty<>(targetLineData);
+    private ObservableList<XYChart.Data<Number, Number>> loggedHoursData = FXCollections.observableArrayList();
+    private ObjectProperty<ObservableList<XYChart.Data<Number, Number>>> loggedHoursDataProperty = new SimpleObjectProperty<>(loggedHoursData);
+    private ObservableList<XYChart.Data<Number, Number>> burndownData = FXCollections.observableArrayList();
+    private ObjectProperty<ObservableList<XYChart.Data<Number, Number>>> burndownDataProperty = new SimpleObjectProperty<>(burndownData);
+
 
     public SprintDetailsPaneBurndownViewModel() {
         super();
     }
 
+    public ObjectProperty<ObservableList<XYChart.Data<Number, Number>>> targetLineDataProperty() {
+        return targetLineDataProperty;
+    }
+
+    public ObjectProperty<ObservableList<XYChart.Data<Number, Number>>> loggedHoursDataProperty() {
+        return loggedHoursDataProperty;
+    }
+
+    public ObjectProperty<ObservableList<XYChart.Data<Number, Number>>> burndownDataProperty() {
+        return burndownDataProperty;
+    }
+
+    //TODO remove this
+    public void buttonPress() {
+        targetLineData.add(new XYChart.Data(10, 20));
+    }
+
     @Override
     public void commitEdit() {
-
+        // no editing done on chart
     }
 
     @Override
     public void cancelEdit() {
-
+        // no editing done on chart
     }
 
 }
