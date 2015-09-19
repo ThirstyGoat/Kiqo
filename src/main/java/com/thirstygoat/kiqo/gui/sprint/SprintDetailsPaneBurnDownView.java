6f461ddeb9a1918dc02c2a995de7eb301df5c302
Sam Schofield <sds53@uclive.ac.nz>
2015-09-19 21:00:26 +1200

    Added burn down chart to the sprint details pane story:286

--- src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurnDownView.java
+++ src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurnDownView.java
@@ -2,7 +2,9 @@ package com.thirstygoat.kiqo.gui.sprint;
 
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.LineChart;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -11,12 +13,12 @@ import java.util.ResourceBundle;
 * Created by Carina Blair on 3/08/2015.
 */
 public class SprintDetailsPaneBurnDownView implements FxmlView<SprintDetailsPaneDetailsViewModel>, Initializable {
-    
 
     @InjectViewModel
     private SprintDetailsPaneDetailsViewModel viewModel;
-    
 
+    @FXML
+    private LineChart<Number, Number> burndownChart;
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
