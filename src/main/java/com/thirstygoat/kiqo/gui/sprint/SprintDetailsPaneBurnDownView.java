603ce487b208e971112bcaf6a5c51fa6b881ab7f
root <root@amy-HP-ENVY-14-SPECTRE-Notebook-PC>
2015-09-21 15:10:49 +1200

    Added "save burndown as image" functionality to Sprint burndown.
    story:286

--- src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurnDownView.java
+++ src/main/java/com/thirstygoat/kiqo/gui/sprint/SprintDetailsPaneBurnDownView.java
@@ -1,15 +1,24 @@
 package com.thirstygoat.kiqo.gui.sprint;
 
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javax.imageio.ImageIO;
+
+import com.thirstygoat.kiqo.gui.MainController;
+
 import de.saxsys.mvvmfx.FxmlView;
 import de.saxsys.mvvmfx.InjectViewModel;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
-
-import java.net.URL;
-import java.util.ResourceBundle;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.stage.FileChooser;
 
 /**
 * Created by Carina Blair on 3/08/2015.
@@ -36,6 +45,25 @@ public class SprintDetailsPaneBurnDownView implements FxmlView<SprintDetailsPane
 
         burndownChart.getData().addAll(targetLineSeries, loggedHoursSeries, burndownSeries);
         burndownChart.getXAxis().setAutoRanging(true);
+        
+        button.setOnAction(event -> {
+        	burndownChart.snapshot(snapshotResult -> {
+        		WritableImage image = snapshotResult.getImage();
+        		// https://community.oracle.com/thread/2450090?tstart=0
+        		final FileChooser fileChooser = new FileChooser();
+                fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("PNG Files", "*.png"), new FileChooser.ExtensionFilter("All Files", "*"));
+                String organisationFilename = viewModel.organisationProperty().get().getSaveLocation().getName();
+				fileChooser.setInitialFileName(organisationFilename.substring(0, organisationFilename.lastIndexOf('.')) + ".png");
+                File file = fileChooser.showSaveDialog(burndownChart.getScene().getWindow());
+        		try {
+					ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+        		return null;
+        	}, null, null);
+        });
 
     }
 
