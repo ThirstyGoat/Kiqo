8f391f8224ce01dc561bdcbb9b5b25365281680d
Leroy Hopson <git@leroy.geek.nz>
2015-09-21 12:57:30 +1200

    WIP

--- src/main/java/com/thirstygoat/kiqo/gui/backlog/BacklogFormViewModel.java
+++ src/main/java/com/thirstygoat/kiqo/gui/backlog/BacklogFormViewModel.java
@@ -2,16 +2,11 @@ package com.thirstygoat.kiqo.gui.backlog;
 
 import com.thirstygoat.kiqo.command.Command;
 import com.thirstygoat.kiqo.command.UndoManager;
-import com.thirstygoat.kiqo.model.Story;
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.SimpleListProperty;
 
 /**
 * Created by Carina Blair on 19/07/2015.
 */
 public class BacklogFormViewModel extends BacklogViewModel {
-    private ListProperty<Story> sourceStoriesProperty = new SimpleListProperty<>();
-
     private Runnable exitStrategy;
 
     public void setExitStrategy(Runnable exitStrategy) {
