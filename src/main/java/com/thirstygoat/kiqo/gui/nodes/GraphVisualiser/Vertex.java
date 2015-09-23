d3c7ff328f5d956b67d7f95471587143f97c301d
Bradley Kirwan <bjk60@uclive.ac.nz>
2015-09-23 23:05:58 +1200

    Added Grandalf Python library which provides the graph spacing algorithm implementation we are using. Added Jython dependency to the pom. Created Edge and Vertex classes to managed edges and vertices within a graph. Created GraphVisualiser which generates graph by running the graph spacing algorithm to determine the position of the vertices within the graph (ties everything together). story:345 pair:[sds53,bjk60]

---
+++ src/main/java/com/thirstygoat/kiqo/gui/nodes/GraphVisualiser/Vertex.java
@@ -0,0 +1,37 @@
+package com.thirstygoat.kiqo.gui.nodes.GraphVisualiser;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+
+/**
+ * Created by bradley on 23/09/15.
+ */
+public class Vertex<T> {
+    private T object;
+    private DoubleProperty xPos = new SimpleDoubleProperty(0);
+    private DoubleProperty yPos = new SimpleDoubleProperty(0);
+
+    public Vertex(T object) {
+        this.object = object;
+    }
+
+    public T getObject() {
+        return object;
+    }
+
+    public DoubleProperty yPosProperty() {
+        return yPos;
+    }
+    public DoubleProperty xPosProperty() {
+        return xPos;
+    }
+
+    @Override
+    public String toString() {
+        return "Vertex{" +
+                "object=" + object +
+                ", xPos=" + xPos +
+                ", yPos=" + yPos +
+                '}';
+    }
+}
