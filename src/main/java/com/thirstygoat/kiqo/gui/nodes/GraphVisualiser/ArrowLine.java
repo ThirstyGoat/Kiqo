00389f8bcdef332d6328d518cfed5e200b0e1c9c
Sam Schofield <sds53@uclive.ac.nz>
2015-09-23 23:26:50 +1200

    Created ArrowLine file story:345

---
+++ src/main/java/com/thirstygoat/kiqo/gui/nodes/GraphVisualiser/ArrowLine.java
@@ -0,0 +1,9 @@
+package com.thirstygoat.kiqo.gui.nodes.GraphVisualiser;
+
+import javafx.scene.shape.Line;
+
+/**
+ * Created by samschofield on 23/09/15.
+ */
+public class ArrowLine extends Line {
+}
