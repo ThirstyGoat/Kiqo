b48e1dd439a0450b1d5486781fb45edd9305c4ea
Sam Schofield <sds53@uclive.ac.nz>
2015-09-24 13:13:26 +1200

    Created methods to separate graph vertices into connected sub graphs Implemented tests to ensure that the methods for splitting graph into connected sub graphs are working correctly.
    story:345 pair[sdsd53, bjk60]

--- src/main/java/com/thirstygoat/kiqo/gui/nodes/GraphVisualiser/GraphVisualiser.java
+++ src/main/java/com/thirstygoat/kiqo/gui/nodes/GraphVisualiser/GraphVisualiser.java
@@ -62,50 +62,6 @@ public class GraphVisualiser<T> extends FlowPane {
         return connectedVertices;
     }
 
-//    public class FloodFillAlgorithm {
-//        public ArrayList<List<Node>> islands;
-//        public ArrayList<Node> nodes;
-//
-//        public static class Node {
-//            public ArrayList<Node> neighbors = new ArrayList<>();
-//            public String label;
-//
-//            public Node(String label) {
-//                this.label = label;
-//            }
-//
-//            @Override
-//            public String toString() {
-//                return label;
-//            }
-//        }
-//
-//        public FloodFillAlgorithm(List<Node> nodes) {
-//            this.nodes = new ArrayList<>();
-//            this.nodes.addAll(nodes);
-//        }
-//
-//        public void floodFill(Node node, List<Node> island) {
-//            if (!island.contains(node)) {
-//                island.add(node);
-//            } else {
-//                return;
-//            }
-//            node.neighbors.forEach(neighbor -> floodFill(neighbor, island));
-//        }
-//
-//        public List<List<Node>> execute() {
-//            islands = new ArrayList<>();
-//            while (nodes.size() > 0) {
-//                ArrayList<Node> island = new ArrayList<>();
-//                floodFill(nodes.get(0), island);
-//                nodes.removeAll(island);
-//                islands.add(island);
-//            }
-//            return islands;
-//        }
-//    }
-
     public void setNodeCallback(Callback<T, Node> nodeCallback) {
         this.nodeCallback = nodeCallback;
     }
