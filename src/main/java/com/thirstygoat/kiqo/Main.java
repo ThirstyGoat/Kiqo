95037b4cea040c870fb9ef0d96e89f97baa27954
jjharrison-uc <jjh72@uclive.ac.nz>
2015-09-24 18:51:09 +1200

    Fixed the burndown data to contain points for EVERY day.
    story: 286

--- src/main/java/com/thirstygoat/kiqo/Main.java
+++ src/main/java/com/thirstygoat/kiqo/Main.java
@@ -136,6 +136,14 @@ public class Main extends Application {
         mainController.setPrimaryStage(primaryStage);
         if (file != null && file.exists()) {
             mainController.openOrganisation(file);
+            Platform.runLater(() -> {
+                MainController.focusedItemProperty
+                                .set(mainController.selectedOrganisationProperty().get().getProjects().get(0)
+                                                .getReleases().get(0).getSprints().get(0));
+                mainController.getDetailsPaneController().showDetailsPane(
+                                mainController.selectedOrganisationProperty().get().getProjects().get(0).getReleases()
+                                                .get(0).getSprints().get(0));
+            });
         }
     }
 }
