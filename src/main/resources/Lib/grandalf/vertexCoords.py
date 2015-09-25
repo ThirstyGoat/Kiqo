f1010325a55a3652f6c725dd7ba5c3af04fe7a3a
Sam Schofield <sds53@uclive.ac.nz>
2015-09-25 15:39:22 +1200

    Updated the pom + the position of the python resources so that dependency visualisation will work when run from the jar + added __init__py files to the grandalf directories so that the files can be accessed
    story:345

---
+++ src/main/resources/Lib/grandalf/vertexCoords.py
@@ -0,0 +1,36 @@
+from grandalf.graphs import Vertex,Edge,Graph
+from grandalf.layouts import DigcoLayout
+
+def createVertex(name, width=10, height=10):
+    return Vertex(name)
+
+
+def createEdge(vertexA, vertexB):
+    return Edge(vertexA, vertexB)
+
+
+def getVertexPositions(vertices, edges):
+    #print vertices
+    #print edges
+
+    # Create the vertices
+    # Create the edges
+    V = vertices
+    E = edges
+
+    # Set the size for each vertex
+    class defaultview(object):
+        w, h = 180, 150
+
+    for v in V: v.view = defaultview()
+
+    g = Graph(V, E)
+    dco = DigcoLayout(g.C[0])
+    dco.init_all()
+    dco.draw(30)
+
+    toReturn = []
+    for v in g.C[0].sV:
+        toReturn.append([v.data, v.view.xy[0], v.view.xy[1]])
+
+    return toReturn
