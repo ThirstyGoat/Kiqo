ee1100a4ff31a42236638162a111a87f22e7e8a9
akm96 <akm96@uclive.ac.nz>
2015-09-25 15:54:31 +1200

    remove unused files and methods (relating to roleBadges in teamMembers detailsPane).
    story:355

--- src/main/java/com/thirstygoat/kiqo/gui/team/TeamDetailsPaneViewModel.java
+++ src/main/java/com/thirstygoat/kiqo/gui/team/TeamDetailsPaneViewModel.java
@@ -1,28 +1,16 @@
 package com.thirstygoat.kiqo.gui.team;
 
-import com.thirstygoat.kiqo.command.Command;
-import com.thirstygoat.kiqo.command.UndoManager;
-import com.thirstygoat.kiqo.gui.Editable;
-import com.thirstygoat.kiqo.gui.MainController;
+import com.thirstygoat.kiqo.command.*;
+import com.thirstygoat.kiqo.gui.*;
 
-import javafx.beans.property.ListProperty;
-import javafx.beans.property.ObjectProperty;
-import javafx.beans.property.SimpleListProperty;
-import javafx.beans.property.SimpleObjectProperty;
-import javafx.collections.FXCollections;
+import javafx.beans.property.*;
 
 public class TeamDetailsPaneViewModel extends TeamViewModel implements Editable {
 
     private ObjectProperty<MainController> mainControllerProperty = new SimpleObjectProperty<>();
-    private ListProperty<PersonListItemViewModel> teamMemberViewModels =
-                    new SimpleListProperty<>(FXCollections.observableArrayList());
 
     public ObjectProperty<MainController> mainControllerProperty() {
         return mainControllerProperty;
-    }
-
-    public ListProperty<PersonListItemViewModel> teamMemberViewModels() {
-        return teamMemberViewModels;
     }
 
     @Override
